43a5bf7868e3576978ba9eeb491056d1af49760a
GanXiKang <gan0612xk@hotmail.com>
2023-02-19 00:29:21 +0800

    feat : 編寫了Dice轉動的程式

--- Assets/Script/DiceControl.cs
+++ Assets/Script/DiceControl.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class DiceControl : MonoBehaviour
 {
+    static Rigidbody rb;
+    public static Vector3 diceVelocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        
+        diceVelocity = rb.velocity;
+
+        if (Input.GetKeyDown("space"))
+        {
+            float dirX = Random.Range(500, 1000);
+            float dirY = Random.Range(500, 1000);
+            float dirZ = Random.Range(500, 1000);
+            
+            transform.rotation = Quaternion.identity;
+            rb.AddForce(transform.up * 1000);
+            rb.AddTorque(dirX, dirY, dirZ);
+        }
     }
 }
