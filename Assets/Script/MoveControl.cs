a39375b9e30abf4bc5392553c13af95b12d4a88d
GanXiKang <gan0612xk@hotmail.com>
2023-02-22 01:26:14 +0800

    feat : 可以通過骰子點數去到指定的位置（行走方式為直綫）

--- Assets/Script/MoveControl.cs
+++ Assets/Script/MoveControl.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveControl : MonoBehaviour
-{
-    public float pointNum = 0;
-
+{ 
     public Transform point1;
     public Transform point2;
     public Transform point3;
@@ -20,41 +18,42 @@ public class MoveControl : MonoBehaviour
     }
     void Update()
     {
-        while(DiceColliderZoneControl.diceNumber > 0)
-        {
-            pointNum++;
+        float n = DiceColliderZoneControl.diceNumber;
 
-            if (pointNum == 1)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point1.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
-            if (pointNum == 2)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point2.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
-            if (pointNum == 3)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point3.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
-            if (pointNum == 4)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point4.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
-            if (pointNum == 5)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point5.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
-            if (pointNum == 6)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, point6.transform.position + new Vector3(0, 6, 0), 0.05f);
-                DiceColliderZoneControl.diceNumber--;
-            }
+
+
+
+        if (n == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point1.transform.position + new Vector3(0, 6, 0), 0.05f);
+
         }
-        
+        if (n == 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point2.transform.position + new Vector3(0, 6, 0), 0.05f);
+
+        }
+        if (n == 3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point3.transform.position + new Vector3(0, 6, 0), 0.05f);
+
+        }
+        if (n == 4)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point4.transform.position + new Vector3(0, 6, 0), 0.05f);
+
+        }
+        if (n == 5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point5.transform.position + new Vector3(0, 6, 0), 0.05f);
+
+        }
+        if (n == 6)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point6.transform.position + new Vector3(0, 6, 0), 0.05f);
+
+        }
+    
+
     }
 }
