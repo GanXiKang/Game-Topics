01a3acd261f939638523a528791351fefabe4f37
GanXiKang <gan0612xk@hotmail.com>
2023-02-26 01:07:11 +0800

    feat : 優化程式碼

--- Assets/Script/MoveControl.cs
+++ Assets/Script/MoveControl.cs
@@ -24,5 +24,25 @@ public class MoveControl : MonoBehaviour
         {
             transform.DOMove(p[5].transform.position + new Vector3(0, 6, 0), 1);
         }
+        if (Dice.diceNum == 2)
+        {
+            transform.DOMove(p[4].transform.position + new Vector3(0, 6, 0), 1);
+        }
+        if (Dice.diceNum == 3)
+        {
+            transform.DOMove(p[3].transform.position + new Vector3(0, 6, 0), 1);
+        }
+        if (Dice.diceNum == 4)
+        {
+            transform.DOMove(p[2].transform.position + new Vector3(0, 6, 0), 1);
+        }
+        if (Dice.diceNum == 5)
+        {
+            transform.DOMove(p[1].transform.position + new Vector3(0, 6, 0), 1);
+        }
+        if (Dice.diceNum == 6)
+        {
+            transform.DOMove(p[0].transform.position + new Vector3(0, 6, 0), 1);
+        }
     }
 }
