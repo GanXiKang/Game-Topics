34c1dfd6433af751793797c5bd881eb7582799a2
GanXiKang <gan0612xk@hotmail.com>
2023-03-05 02:22:13 +0800

    feat : 優化路綫失敗

--- Assets/Script/MoveControl.cs
+++ Assets/Script/MoveControl.cs
@@ -7,13 +7,23 @@ using UnityEngine.AI;
 public class MoveControl : MonoBehaviour
 {
     public float recordNum = 0;            
-
     public GameObject[] p;
+
+    private NavMeshAgent agent;
 
     void Start()
     {
-        p = GameObject.FindGameObjectsWithTag("Point");
+        agent = GetComponent<NavMeshAgent>();
         DOTween.SetTweensCapacity(500, 125);
+
+        //p[0] = GameObject.FindGameObjectWithTag("Point");
+        //p[1] = GameObject.FindGameObjectWithTag("Point1");
+        //p[2] = GameObject.FindGameObjectWithTag("Point2");
+        //p[3] = GameObject.FindGameObjectWithTag("Point3");
+        //p[4] = GameObject.FindGameObjectWithTag("Point4");
+        //p[5] = GameObject.FindGameObjectWithTag("Point5");
+        //p[6] = GameObject.FindGameObjectWithTag("Point6");
+        //p[7] = GameObject.FindGameObjectWithTag("Point7");
     }
     void Update()
     {
@@ -24,31 +34,31 @@ public class MoveControl : MonoBehaviour
     {
         if (Dice.totalNum == 1)
         {
-            transform.DOMove(p[6].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[1].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum == 2)
         {
-            transform.DOMove(p[5].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[2].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum == 3)
         {
-            transform.DOMove(p[4].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[3].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum == 4)
         {
-            transform.DOMove(p[3].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[4].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum == 5)
         {
-            transform.DOMove(p[2].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[5].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum == 6)
         {
-            transform.DOMove(p[1].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[6].transform.position + new Vector3(0, 6, 0), 1);
         }
         if (Dice.totalNum > 6)
         {
-            transform.DOMove(p[0].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[7].transform.position + new Vector3(0, 6, 0), 1);
         }
     }
     void MoveDicePoint()
@@ -81,5 +91,9 @@ public class MoveControl : MonoBehaviour
         {
             transform.DOMove(p[0].transform.position + new Vector3(0, 6, 0), 1);
         }
+    }
+    void NavDestination(int a)
+    {
+        agent.SetDestination(p[a].transform.position);
     }
 }
