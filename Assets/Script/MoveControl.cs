92ed7a87bc539fb1c98a13c3371deb606807cc8b
GanXiKang <gan0612xk@hotmail.com>
2023-03-08 22:25:02 +0800

    feat ： 程式優化

--- Assets/Script/MoveControl.cs
+++ Assets/Script/MoveControl.cs
@@ -6,14 +6,13 @@ using DG.Tweening;
 public class MoveControl : MonoBehaviour
 {
     public float recordNum = 0;            
-    public GameObject[] p;
+    public Transform[] p = new Transform[8];
 
     private float pointNum = 0;
 
     void Start()
     {
         DOTween.SetTweensCapacity(500, 125);
-        p = GameObject.FindGameObjectsWithTag("Point");
     }
     void FixedUpdate()
     {
@@ -24,31 +23,31 @@ public class MoveControl : MonoBehaviour
     {
         if (Dice.totalNum == 1)
         {
-            transform.DOMove(p[6].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[1].transform.position, 1);
         }
         if (Dice.totalNum == 2)
         {
-            transform.DOMove(p[5].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[2].transform.position, 1);
         }
         if (Dice.totalNum == 3)
         {
-            transform.DOMove(p[4].transform.position + new Vector3(0, 6, 0), 3);
+            transform.DOMove(p[3].transform.position, 3);
         }
         if (Dice.totalNum == 4)
         {
-            transform.DOMove(p[3].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[4].transform.position, 1);
         }
         if (Dice.totalNum == 5)
         {
-            transform.DOMove(p[2].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[5].transform.position, 1);
         }
         if (Dice.totalNum == 6)
         {
-            transform.DOMove(p[1].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[6].transform.position, 1);
         }
         if (Dice.totalNum > 6)
         {
-            transform.DOMove(p[0].transform.position + new Vector3(0, 6, 0), 1);
+            transform.DOMove(p[7].transform.position, 1);
         }
     }
     void MoveDicePoint()
