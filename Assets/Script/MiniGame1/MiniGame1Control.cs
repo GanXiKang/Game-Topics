673b1602dfff2338a497c2b82eb60de0ce7a6ff9
GanXiKang <gan0612xk@hotmail.com>
2023-03-12 00:21:26 +0800

    feat ： 建立小游戲程式碼

---
+++ Assets/Script/MiniGame1/MiniGame1Control.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame1Control : MonoBehaviour
+{
+    void Awake()
+    {
+        print("ÀêØˆ²ÂÈ­£¡");
+    }
+    void Update()
+    {
+        
+    }
+}
