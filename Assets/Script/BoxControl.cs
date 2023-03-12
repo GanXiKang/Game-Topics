159e72d719acd3ac0d47efab1ca19fb9e0ac6b01
GanXiKang <gan0612xk@hotmail.com>
2023-03-13 01:15:47 +0800

    feat ： 簡化開寶箱程式碼

--- Assets/Script/BoxControl.cs
+++ Assets/Script/BoxControl.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class BoxControl : MonoBehaviour
 {
     public Transform unopen;
-    public Transform open;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && Dice.totalNum == 2)
         {
             print("¹§Ï²«@µÃ10ØˆØˆŽÅ£¡");
-            Vector3.MoveTowards(unopen.transform.position, open.transform.position, 0.3f);
-            this.gameObject.SetActive(false);
-            Destroy(this.gameObject);
+            unopen.transform.Translate(0, 5, 0);
+            Destroy(this.gameObject, 2f);
         }
     }
 }
