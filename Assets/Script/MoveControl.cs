9893faa4345cd763fbb64406cd1a4cd16a1f6293
GanXiKang <gan0612xk@hotmail.com>
2023-03-19 19:26:31 +0800

    feat ： 把到達目的地的時間拉長

--- Assets/Script/MoveControl.cs
+++ Assets/Script/MoveControl.cs
@@ -32,37 +32,37 @@ public class MoveControl : MonoBehaviour
         if (Dice.totalNum == 1)
         {
             //agent.SetDestination(p[1].transform.position);
-            transform.DOMove(p[1].transform.position, 4);
+            transform.DOMove(p[1].transform.position, 10);
             Award = false;
         }
         if (Dice.totalNum == 2)
         {
-            transform.DOMove(p[2].transform.position, 4);
+            transform.DOMove(p[2].transform.position, 10);
             Award = true;
         }
         if (Dice.totalNum == 3)
         {
-            transform.DOMove(p[3].transform.position, 4);
+            transform.DOMove(p[3].transform.position, 10);
             Award = false;
         }
         if (Dice.totalNum == 4)
         {
-            transform.DOMove(p[4].transform.position, 4);
+            transform.DOMove(p[4].transform.position, 10);
             Award = false;
         }
         if (Dice.totalNum == 5)
         {
-            transform.DOMove(p[5].transform.position, 4);
+            transform.DOMove(p[5].transform.position, 10);
             Award = true;
         }
         if (Dice.totalNum == 6)
         {
-            transform.DOMove(p[6].transform.position, 4);
+            transform.DOMove(p[6].transform.position, 10);
             Award = false;
         }
         if (Dice.totalNum > 6)
         {
-            transform.DOMove(p[7].transform.position, 4);
+            transform.DOMove(p[7].transform.position, 10);
             Award = false;
         }
     }
