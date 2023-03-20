dd90dcf68b74ea0e87384f445f69f4593af8244e
GanXiKang <gan0612xk@hotmail.com>
2023-03-21 01:26:18 +0800

    feat : 編寫了游戲倒計時時間

--- Assets/Script/MiniGame2/MG2_UIControl.cs
+++ Assets/Script/MiniGame2/MG2_UIControl.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MG2_UIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text GameTime;
+    public Text Score;
+
+    float gameTime = 30f;
+    float score = 0;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameTime >= 0)
+        {
+            gameTime -= 1 * Time.deltaTime;
+            GameTime.text = "Time : " + gameTime.ToString("f0") + "s";
+        }
     }
 }
