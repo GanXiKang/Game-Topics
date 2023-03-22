44d304d7403e132a8d9d01f5a3a793d6ce739429
GanXiKang <gan0612xk@hotmail.com>
2023-03-22 23:43:07 +0800

    feat ： box之間會互換

--- Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
+++ Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
@@ -19,12 +19,19 @@ public class MG3_BoxMoveControl : MonoBehaviour
         if (PlayGameNum == 1)
         {
             DownAllBox();
+            MoveBox();
         }
     }
     void DownAllBox()
     {
-        box1.transform.position = new Vector3(-6, 2.5f, -5);
-        box2.transform.position = new Vector3(0, 2.5f, -5);
-        box3.transform.position = new Vector3(6, 2.5f, -5);
+        box1.transform.position = boxPoint1.transform.position;
+        box2.transform.position = boxPoint2.transform.position;
+        box3.transform.position = boxPoint3.transform.position;
+    }
+    void MoveBox()
+    {
+        box1.transform.position = boxPoint2.transform.position;
+        box2.transform.position = boxPoint1.transform.position;
     }
 }
+
