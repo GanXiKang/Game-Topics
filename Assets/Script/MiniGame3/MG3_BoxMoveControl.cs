c6e2dc264f82e840d61243de784a88e390312f68
GanXiKang <gan0612xk@hotmail.com>
2023-03-23 00:11:34 +0800

    feat ： 編寫選擇要open的box

--- Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
+++ Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
@@ -14,12 +14,28 @@ public class MG3_BoxMoveControl : MonoBehaviour
     public Transform boxPoint2;
     public Transform boxPoint3;
 
+    public static bool openBox1;
+    public static bool openBox2;
+    public static bool openBox3;
+
     void Update()
     {
         if (PlayGameNum == 1)
         {
             DownAllBox();
             MoveBox();
+        }
+        if (openBox1 == true)
+        {
+            box1.transform.position += new Vector3(0, 2.5f, 0);
+        }
+        if (openBox2 == true)
+        {
+            box2.transform.position += new Vector3(0, 2.5f, 0);
+        }
+        if (openBox3 == true)
+        {
+            box3.transform.position += new Vector3(0, 2.5f, 0);
         }
     }
     void DownAllBox()
--- Assets/Script/MiniGame3/MG3_ButtonControl.cs
+++ Assets/Script/MiniGame3/MG3_ButtonControl.cs
@@ -12,18 +12,21 @@ public class MG3_ButtonControl : MonoBehaviour
     public void StartControl()
     {
         B_Start.gameObject.SetActive(false);
+        B_Box1.gameObject.SetActive(true);
+        B_Box2.gameObject.SetActive(true);
+        B_Box3.gameObject.SetActive(true);
         MG3_BoxMoveControl.PlayGameNum++;
     }
     public void OpenBox1()
     {
-        
+        MG3_BoxMoveControl.openBox1 = true;
     }
     public void OpenBox2()
     {
-
+        MG3_BoxMoveControl.openBox2 = true;
     }
     public void OpenBox3()
     {
-
+        MG3_BoxMoveControl.openBox3 = true;
     }
 }
