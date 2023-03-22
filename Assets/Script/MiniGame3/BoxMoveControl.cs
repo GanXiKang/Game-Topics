efda690e4b0cf6980110c3bac1fb5b975ffeb6f1
GanXiKang <gan0612xk@hotmail.com>
2023-03-22 22:36:18 +0800

    feat ： 建立箱子移動的程式

---
+++ Assets/Script/MiniGame3/BoxMoveControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMoveControl : MonoBehaviour
+{
+    public GameObject box1;
+    public GameObject box2; 
+    public GameObject box3;
+
+    void Update()
+    {
+        
+    }
+}
