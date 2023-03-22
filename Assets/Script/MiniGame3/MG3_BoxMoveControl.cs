938237f7ffb31e7c4a838dcda531aa5466eba15f
GanXiKang <gan0612xk@hotmail.com>
2023-03-22 22:38:30 +0800

    fix : 更換名字

--- Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
+++ Assets/Script/MiniGame3/MG3_BoxMoveControl.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxMoveControl : MonoBehaviour
+public class MG3_BoxMoveControl : MonoBehaviour
 {
     public GameObject box1;
-    public GameObject box2; 
+    public GameObject box2;
     public GameObject box3;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
