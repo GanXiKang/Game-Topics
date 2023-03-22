4508c108d2e831d3213d04d45493df2a5be40e6f
GanXiKang <gan0612xk@hotmail.com>
2023-03-22 22:32:53 +0800

    feat ： 編寫Button控制程式

---
+++ Assets/Script/MiniGame3/MG3_ButtonControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MG3_ButtonControl : MonoBehaviour
+{
+    public GameObject B_Start;
+    public GameObject B_Box1;
+    public GameObject B_Box2;
+    public GameObject B_Box3;
+    public void StartControl()
+    {
+        B_Start.gameObject.SetActive(false);
+    }
+    public void OpenBox1()
+    {
+        
+    }
+    public void OpenBox2()
+    {
+
+    }
+    public void OpenBox3()
+    {
+
+    }
+}
