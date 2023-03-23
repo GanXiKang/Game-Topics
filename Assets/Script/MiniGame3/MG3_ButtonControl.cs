6e3e5b241e6b7d4586894ac4b87b24d3ad114c43
GanXiKang <gan0612xk@hotmail.com>
2023-03-24 00:53:35 +0800

    feat : 編寫按鈕的控制

--- Assets/Script/MiniGame3/MG3_ButtonControl.cs
+++ Assets/Script/MiniGame3/MG3_ButtonControl.cs
@@ -9,8 +9,13 @@ public class MG3_ButtonControl : MonoBehaviour
     public GameObject B_Box1;
     public GameObject B_Box2;
     public GameObject B_Box3;
+
+    public static float openWhyBox;
+    public static float whyRound;
+
     public void StartControl()
     {
+        whyRound++;
         B_Start.gameObject.SetActive(false);
         B_Box1.gameObject.SetActive(true);
         B_Box2.gameObject.SetActive(true);
@@ -21,13 +26,26 @@ public class MG3_ButtonControl : MonoBehaviour
     public void OpenBox1()
     {
         MG3_BoxMoveControl.openBox1 = true;
+        openWhyBox = 1;
+        B_Box1.gameObject.SetActive(false);
+        B_Box2.gameObject.SetActive(false);
+        B_Box3.gameObject.SetActive(false);
     }
     public void OpenBox2()
     {
         MG3_BoxMoveControl.openBox2 = true;
+        openWhyBox = 2;
+        B_Box1.gameObject.SetActive(false);
+        B_Box2.gameObject.SetActive(false);
+        B_Box3.gameObject.SetActive(false);
     }
     public void OpenBox3()
     {
         MG3_BoxMoveControl.openBox3 = true;
+        openWhyBox = 3;
+        B_Box1.gameObject.SetActive(false);
+        B_Box2.gameObject.SetActive(false);
+        B_Box3.gameObject.SetActive(false);
     }
+
 }
