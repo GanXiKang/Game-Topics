c71a9d1bafef30988da35d0bdf41eafa6402e188
GanXiKang <gan0612xk@hotmail.com>
2023-03-24 00:27:18 +0800

    feat ： 創建星星的程式

---
+++ Assets/Script/MiniGame3/MG3_StarControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG3_StarControl : MonoBehaviour
+{
+    public GameObject star;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
