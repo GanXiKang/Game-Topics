ba6396de4c613e8a3e34207db95e2a489bc1eae6
GanXiKang <gan0612xk@hotmail.com>
2023-03-25 02:37:11 +0800

    feat ： 編寫Cup碰到Star會Destory (BUG)

--- Assets/Script/MiniGame3/MG3_DestoryControl.cs
+++ Assets/Script/MiniGame3/MG3_DestoryControl.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class MG3_DestoryControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.tag == "Star")
+        {
+            print("DestoryStar");
+            Destroy(other.gameObject);
+        }
     }
 }
