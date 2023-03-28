9928abe19504288f1dfac942a6d0b2abff87d77c
GanXiKang <gan0612xk@hotmail.com>
2023-03-29 02:42:35 +0800

    feat ： 測試Vector3的可以移動

--- Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
+++ Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
@@ -6,7 +6,7 @@ public class MG4_PlayerMoveControl : MonoBehaviour
 {
     Rigidbody rb;
 
-    public float speed = 5;
+    public float speed = 15;
     public float jump = 10;
     void Start()
     {
@@ -14,8 +14,8 @@ public class MG4_PlayerMoveControl : MonoBehaviour
     }
     void Update()
     {
-        rb.AddForce(0, 0, speed * Time.deltaTime);
-        //transform.Translate(0, 0, speed * Time.deltaTime);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        transform.Translate(0, 0, speed * Time.deltaTime);
         if (Input.GetKeyDown("w"))
         {
             rb.velocity = Vector3.up * jump;
