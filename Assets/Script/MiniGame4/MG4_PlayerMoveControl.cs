0b7e1316696605ac58ab49b5e60747f396b8157f
GanXiKang <gan0612xk@hotmail.com>
2023-03-29 02:13:39 +0800

    feat ： 編寫了jump

--- Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
+++ Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
@@ -6,7 +6,8 @@ public class MG4_PlayerMoveControl : MonoBehaviour
 {
     Rigidbody rb;
 
-    public float speed = 100;
+    public float speed = 60;
+    public float jump = 10;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -14,5 +15,9 @@ public class MG4_PlayerMoveControl : MonoBehaviour
     void Update()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+        if (Input.GetKeyDown("w"))
+        {
+            rb.velocity = Vector3.up * jump;
+        }
     }
 }
