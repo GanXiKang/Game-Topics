a4f08636d04dd45f79d17bec885852f5ede24313
GanXiKang <gan0612xk@hotmail.com>
2023-03-30 22:51:34 +0800

    feat : 優化程式

--- Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
+++ Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
@@ -6,16 +6,15 @@ public class MG4_PlayerMoveControl : MonoBehaviour
 {
     Rigidbody rb;
 
-    public float speed = 15;
-    public float jump = 10;
+    float speed = 5;
+    float jump = 12;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        rb.AddForce(speed * transform.forward);
         if (Input.GetKeyDown("w"))
         {
             rb.velocity = Vector3.up * jump;
