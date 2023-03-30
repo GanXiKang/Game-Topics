420974286a3c641b2e1b2eaa2e9dcadd96b56c77
GanXiKang <gan0612xk@hotmail.com>
2023-03-31 00:11:48 +0800

    feat ： 對物件位置&大小調整

--- Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
+++ Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
@@ -5,42 +5,17 @@ using UnityEngine;
 public class MG4_PlayerMoveControl : MonoBehaviour
 {
     Rigidbody rb;
-
-    float speed = 20;
     float jump = 12;
-    bool isFloor;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        if (isFloor == true)
+        rb.AddForce(transform.forward);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(transform.forward);
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                transform.Translate(0, 0, speed * Time.deltaTime);
-                rb.velocity = Vector3.up * jump;
-            }
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Floor")
-        {
-            isFloor = true;
-        }
-        if (other.tag == "End")
-        {
-            isFloor = false;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Floor")
-        {
-            isFloor = false;
+            rb.velocity = Vector3.up * jump;
         }
     }
 }
