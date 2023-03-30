e9711bc96000036ad9665066b0cf7c99fc25ffb0
GanXiKang <gan0612xk@hotmail.com>
2023-03-30 23:01:59 +0800

    feat : 編寫是否走在地板 & 跳起來向前移動一些

--- Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
+++ Assets/Script/MiniGame4/MG4_PlayerMoveControl.cs
@@ -8,16 +8,35 @@ public class MG4_PlayerMoveControl : MonoBehaviour
 
     float speed = 5;
     float jump = 12;
+    bool isFloor;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        rb.AddForce(speed * transform.forward);
-        if (Input.GetKeyDown("w"))
+        if (isFloor == true)
         {
-            rb.velocity = Vector3.up * jump;
+            rb.AddForce(speed * transform.forward);
+            if (Input.GetKeyDown("w"))
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);
+                rb.velocity = Vector3.up * jump;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Floor")
+        {
+            isFloor = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Floor")
+        {
+            isFloor = false;
         }
     }
 }
