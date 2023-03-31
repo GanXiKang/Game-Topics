ab0fde4899c9f79f2c07bb386bf03cfb784467b6
GanXiKang <gan0612xk@hotmail.com>
2023-04-01 04:07:58 +0800

    feat ： 調整loading時間方便測試

--- Assets/Script/SampleScene/UI_LoadingControl.cs
+++ Assets/Script/SampleScene/UI_LoadingControl.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class UI_LoadingControl : MonoBehaviour
 {
-    public float timePeriod = 5;
     float timer;
     void Update()
     {
         timer += 1 * Time.deltaTime;
 
-        if (timer > timePeriod)
+        if (timer >= 1)
         {
             Destroy(this.gameObject);
         }
