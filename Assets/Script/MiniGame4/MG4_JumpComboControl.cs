3e264c84962d24e903a5114277a2b5b31e862b8a
GanXiKang <gan0612xk@hotmail.com>
2023-04-01 23:30:01 +0800

    feat ： 編寫記錄JumpCombo

--- Assets/Script/MiniGame4/MG4_JumpComboControl.cs
+++ Assets/Script/MiniGame4/MG4_JumpComboControl.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MG4_JumpComboControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float combo = 0;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "RecordJump")
+        {
+            combo++;
+            print(combo);
+        }
     }
 }
