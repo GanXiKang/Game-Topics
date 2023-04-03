8912c92245ed5916a012fc2ccc91e8a3b86af09b
GanXiKang <gan0612xk@hotmail.com>
2023-04-03 17:26:21 +0800

    feat ： 編寫AI自動Jump

--- Assets/Script/MiniGame4/MG4_AIControl.cs
+++ Assets/Script/MiniGame4/MG4_AIControl.cs
@@ -7,6 +7,8 @@ public class MG4_AIControl : MonoBehaviour
     Rigidbody rb;
 
     float speed = 18;
+    float jump = 10;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -14,5 +16,12 @@ public class MG4_AIControl : MonoBehaviour
     void FixedUpdate()
     {
         rb.AddForce(transform.forward * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RoadBlock")
+        {
+            rb.velocity = Vector3.up * jump;
+        }
     }
 }
