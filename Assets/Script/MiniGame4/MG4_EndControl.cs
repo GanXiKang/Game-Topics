af6774343dd0072e3afe4ff18be6d5989af42a2b
GanXiKang <gan0612xk@hotmail.com>
2023-04-03 17:50:42 +0800

    feat : 編寫判斷誰先到終點

--- Assets/Script/MiniGame4/MG4_EndControl.cs
+++ Assets/Script/MiniGame4/MG4_EndControl.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MG4_EndControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    float arriveEnd = 0;
+    bool win;
     void Update()
     {
-        
+        if (arriveEnd == 2)
+        {
+            if (win == true)
+            {
+                print("Win");
+            }
+            else 
+            {
+                print("Lose");
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {                                           //判斷誰最后到
+        if (other.tag == "Player")              
+        {
+            print("1");
+            win = false;                        //AI先到終點
+            arriveEnd++;
+        }
+        if (other.tag == "AI")
+        {
+            print("2");
+            win = true;                         //Player先到終點
+            arriveEnd++;
+        }
     }
 }
