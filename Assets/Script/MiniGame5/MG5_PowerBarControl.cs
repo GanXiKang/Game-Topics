faa4ea1da9de8187325cfa4ce26173ee53f83858
GanXiKang <gan0612xk@hotmail.com>
2023-04-05 16:22:59 +0800

    feat ：編寫能量條系統

--- Assets/Script/MiniGame5/MG5_PowerBarControl.cs
+++ Assets/Script/MiniGame5/MG5_PowerBarControl.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MG5_PowerBarControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject powerBar;
+    public Image power;
+
+    float value = 0;
+    bool add = true;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (value >= 5)
+        {
+            add = false;
+            value = 5;
+        }
+        if (value <= 0)
+        {
+            add = true;
+            value = 0;
+        }
+        power.rectTransform.localScale = new Vector3(1, value, 1);
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (add == true)
+            {
+                value += 0.2f;
+            }
+            else
+            {
+                value -= 0.2f;
+            }
+
+        }
     }
 }
