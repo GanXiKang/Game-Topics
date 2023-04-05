449d732a3d2beca3e333f260cabc97daf797f0dd
GanXiKang <gan0612xk@hotmail.com>
2023-04-05 21:27:53 +0800

    feat ： 編寫回之前版本

--- Assets/Script/MiniGame5/MG5_PowerBarControl.cs
+++ Assets/Script/MiniGame5/MG5_PowerBarControl.cs
@@ -29,7 +29,6 @@ public class MG5_PowerBarControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            powerBar.SetActive(true);
             if (add == true)
             {
                 value += 0.1f;
@@ -40,15 +39,6 @@ public class MG5_PowerBarControl : MonoBehaviour
             }
 
         }
-        else
-        {
-            StartCoroutine(PowerBarActive());
-        }
-    }
-    IEnumerator PowerBarActive()
-    {
-        yield return new WaitForSeconds(2f);
-        powerBar.SetActive(false);
     }
 }
 
