285d7c91bd6e061a50e8057120e1bd724e1fb8b0
GanXiKang <gan0612xk@hotmail.com>
2023-04-05 23:17:25 +0800

    feat ： 又做了一題數學

--- Assets/Script/MiniGame5/MG5_HookControl.cs
+++ Assets/Script/MiniGame5/MG5_HookControl.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MG5_HookControl : MonoBehaviour
 {
-    public Transform hookInSitu;
     float depth;
+    float target;
+    Vector3 originalPosition;
 
+    void Start()
+    {
+        originalPosition = this.transform.position;
+    }
     void Update()
     {
         if (MG5_CameraControl.watchPlayer == false)
         {
             depth = 300 * MG5_PowerBarControl.value / 5;
             print(depth);
+            target = transform.position.y - depth;
+            print(target);
         }
     }
 }
