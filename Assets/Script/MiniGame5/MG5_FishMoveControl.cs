83e425bbfbc5fa6f720f36b7d5899ae1cd5bedf7
GanXiKang <gan0612xk@hotmail.com>
2023-04-05 17:12:00 +0800

    feat ： 編寫魚移動程式

--- Assets/Script/MiniGame5/MG5_FishMoveControl.cs
+++ Assets/Script/MiniGame5/MG5_FishMoveControl.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MG5_FishMoveControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float timer;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer < 4f)
+        {
+            transform.position = new Vector3(transform.position.x + 1f * Time.deltaTime, transform.position.y, transform.position.z);
+
+            Quaternion targetR = Quaternion.Euler(0, 180, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetR, 0.3f);
+        }
+        if (timer > 4f && timer < 8f)
+        {
+            transform.position = new Vector3(transform.position.x - 1f * Time.deltaTime, transform.position.y, transform.position.z);
+
+            Quaternion targetR = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetR, 0.3f);
+        }
+        if (timer > 8f)
+        {
+            timer = 0;
+        }
     }
 }
