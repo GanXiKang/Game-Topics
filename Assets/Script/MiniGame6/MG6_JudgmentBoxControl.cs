0ea076f163b68dbff7970d20e9a37da1b15e94c7
GanXiKang <gan0612xk@hotmail.com>
2023-04-09 12:18:33 +0800

    feat ： 編寫變化判斷框

--- Assets/Script/MiniGame6/MG6_JudgmentBoxControl.cs
+++ Assets/Script/MiniGame6/MG6_JudgmentBoxControl.cs
@@ -8,4 +8,30 @@ public class MG6_JudgmentBoxControl : MonoBehaviour
     public Image left;
     public Image right;
 
+    int j = 1;
+
+    void Update()
+    {
+        switch (j)
+        {
+            case 1:
+                left.rectTransform.anchoredPosition = new Vector3(-200, 370, 0);
+                right.rectTransform.anchoredPosition = new Vector3(200, 370, 0);
+                break;
+
+            case 2:
+                left.rectTransform.anchoredPosition = new Vector3(-100, 370, 0);
+                right.rectTransform.anchoredPosition = new Vector3(100, 370, 0);
+                break;
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            j++;
+            print(j);
+        }
+    }
 }
