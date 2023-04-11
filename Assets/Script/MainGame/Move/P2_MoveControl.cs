4a8ad2bdaeeb9a60bcfd07ea231fb771009faf1d
GanXiKang <gan0612xk@hotmail.com>
2023-04-12 02:33:52 +0800

    feat ： 編寫不同玩家的move

--- Assets/Script/MainGame/Move/P2_MoveControl.cs
+++ Assets/Script/MainGame/Move/P2_MoveControl.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class P2_MoveControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] p = new Transform[8];
+
+    public static bool Award;                    //false : Wave , True : Jump
+
     void Start()
     {
-        
+        DOTween.SetTweensCapacity(500, 125);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePoint();                        //ÎÄ×Ö÷»×ÓÓÃ
+    }
+    void MovePoint()
+    {
+        if (Dice.P2_totalNum == 0)
+        {
+            transform.DOMove(p[0].transform.position, 1);
+        }
+        if (Dice.P2_totalNum == 1)
+        {
+            transform.DOMove(p[1].transform.position, 5);
+            Award = false;
+        }
+        if (Dice.P2_totalNum == 2)
+        {
+            transform.DOMove(p[2].transform.position, 5);
+            Award = true;
+        }
+        if (Dice.P2_totalNum == 3)
+        {
+            transform.DOMove(p[3].transform.position, 5);
+            Award = false;
+        }
+        if (Dice.P2_totalNum == 4)
+        {
+            transform.DOMove(p[4].transform.position, 5);
+            Award = false;
+        }
+        if (Dice.P2_totalNum == 5)
+        {
+            transform.DOMove(p[5].transform.position, 5);
+            Award = true;
+        }
+        if (Dice.P2_totalNum == 6)
+        {
+            transform.DOMove(p[6].transform.position, 5);
+            Award = false;
+        }
+        if (Dice.P2_totalNum > 6)
+        {
+            transform.DOMove(p[7].transform.position, 5);
+            Award = false;
+        }
     }
 }
