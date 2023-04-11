7871f6a7cc06b363f0025eb73ebf889fb6647d80
GanXiKang <gan0612xk@hotmail.com>
2023-04-12 02:04:47 +0800

    feat ： 同下

--- Assets/Script/MainGame/Move/MoveControl.cs
+++ Assets/Script/MainGame/Move/MoveControl.cs
@@ -11,17 +11,14 @@ public class MoveControl : MonoBehaviour
     public static bool Award;                    //false : Wave , True : Jump
 
     Rigidbody rb;
-    //NavMeshAgent agent;
 
     void Start()
     {
         DOTween.SetTweensCapacity(500, 125);
-        //agent = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
         MovePoint();                        //文字骰子用
-        //MoveDicePoint();                 //3D骰子用
     }
     void MovePoint()
     {
@@ -31,7 +28,6 @@ public class MoveControl : MonoBehaviour
         }
         if (Dice.totalNum == 1)
         {
-            //agent.SetDestination(p[1].transform.position);
             transform.DOMove(p[1].transform.position, 5);
             Award = false;
         }
@@ -66,36 +62,4 @@ public class MoveControl : MonoBehaviour
             Award = false;
         }
     }
-
-    //void MoveDicePoint()
-    //{
-    //    if (DiceColliderZoneControl.totalNum == 1)
-    //    {
-    //        transform.DOMove(p[6].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum == 2)
-    //    {
-    //        transform.DOMove(p[5].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum == 3)
-    //    {
-    //        transform.DOMove(p[4].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum == 4)
-    //    {
-    //        transform.DOMove(p[3].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum == 5)
-    //    {
-    //        transform.DOMove(p[2].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum == 6)
-    //    {
-    //        transform.DOMove(p[1].transform.position, 2);
-    //    }
-    //    if (DiceColliderZoneControl.totalNum > 6)
-    //    {
-    //        transform.DOMove(p[0].transform.position, 2);
-    //    }
-    //}
 }
