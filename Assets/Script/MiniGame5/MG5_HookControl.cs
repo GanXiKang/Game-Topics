eeddd1f05c2c17e29519f5992b64d14cff414ac4
GanXiKang <gan0612xk@hotmail.com>
2023-04-18 01:10:58 +0800

    feat ： 控制hook上下

--- Assets/Script/MiniGame5/MG5_HookControl.cs
+++ Assets/Script/MiniGame5/MG5_HookControl.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class MG5_HookControl : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 originalLocation;
+
     float speed = 20, y;
-    bool hookDown;
+    bool hookDown, hookUp;
 
     void Start()
     { 
         rb = GetComponent<Rigidbody>();
+        originalLocation = transform.position;
     }
     void Update()
     {
         y = transform.position.y;
+
         if (Input.GetMouseButton(0))
         {
             hookDown = true;
@@ -23,14 +27,29 @@ public class MG5_HookControl : MonoBehaviour
         {
             hookDown = false;
         }
+        if (Input.GetMouseButton(1))
+        {
+            hookUp = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            hookUp = false;
+        }
     }
     void FixedUpdate()
     {
         if (hookDown == true)
         {
-            if (y >= 72)
+            if (y > 72)
             {
                 transform.position = new Vector3(transform.position.x, y - speed * Time.deltaTime, transform.position.z);
+            }
+        }
+        if (hookUp == true)
+        {
+            if (y < 155)
+            {
+                transform.position = new Vector3(transform.position.x, y + speed * Time.deltaTime, transform.position.z);
             }
         }
     }
