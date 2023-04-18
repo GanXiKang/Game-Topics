0286da1734c3b92ffd9e1d095d012c6d3041fbcc
GanXiKang <gan0612xk@hotmail.com>
2023-04-19 01:33:11 +0800

    feat ： 創建角色移動的程式

---
+++ Assets/Script/MiniGame5/MG5_PlayerMoveControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG5_PlayerMoveControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
