c4f2d67bf5f3b3cd84bf35b6d2e7b8f7386cd0f0
GanXiKang <gan0612xk@hotmail.com>
2023-04-19 02:54:12 +0800

    feat ： 找不到bug

--- Assets/Script/MiniGame5/MG5_ScoreControl.cs
+++ Assets/Script/MiniGame5/MG5_ScoreControl.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class MG5_ScoreControl : MonoBehaviour
 {
-    public static float score;
+    public static float score = 0;
 
 
+    void FixedUpdate()
+    {
+        print(score);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "SmallFish")
         {
             score++;
-            print(score);
-        }
-        if (other.tag == "MidFish")
-        {
-            score += 3;
-            Destroy(other.gameObject);
-        }
-        if (other.tag == "BigFish")
-        {
-            score += 5;
-            Destroy(other.gameObject);
-        }
-        if (other.tag == "Rubbish")
-        {
-            score -= 5;
-            Destroy(other.gameObject);
         }
     }
 }
