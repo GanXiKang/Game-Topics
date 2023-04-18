e39b2ee956bfba0f70eab0c251babc6b416d224a
GanXiKang <gan0612xk@hotmail.com>
2023-04-19 01:52:31 +0800

    feat ： 創建一個魚被Caught的程式

---
+++ Assets/Script/MiniGame5/MG5_CaughtControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG5_CaughtControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
