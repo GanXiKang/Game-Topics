c7740cfec46c60dda23acb219ae4d159cc32dfe9
GanXiKang <gan0612xk@hotmail.com>
2023-04-20 02:02:59 +0800

    feat ： 編寫時間和分數控制

--- Assets/Script/MiniGame5/MG5_UIControl.cs
+++ Assets/Script/MiniGame5/MG5_UIControl.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MG5_UIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Text timer, score;
+    public static float gameTime = 45f;
+    
     void Update()
     {
-        
+        if (gameTime >= 0)
+        {
+            gameTime -= 1 * Time.deltaTime;
+            timer.text = "Time:" + gameTime.ToString("f0") + "s";
+        }
+
+        score.text = "Score:" + MG5_HookControl.score;
     }
 }
