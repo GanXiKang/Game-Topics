f3ba6f18518bd83d5b3f4d6a76e6d1862c9b01e0
GanXiKang <gan0612xk@hotmail.com>
2023-04-20 02:10:19 +0800

    feat ： 新名字

---
+++ Assets/Script/MiniGame5/MG5_GameResultControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MG5_GameResultControl : MonoBehaviour
+{
+    void Update()
+    {
+        if (MG5_UIControl.gameTime <= 0)
+        {
+            if (MG5_HookControl.score >= 15)
+            {
+                print("Win");
+            }
+            else
+            {
+                print("Lose");
+            }
+            StartCoroutine(BackMainGame());
+        }
+    }
+    IEnumerator BackMainGame()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);
+    }
+}
