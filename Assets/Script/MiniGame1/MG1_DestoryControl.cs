194f032ca7ac2396b88e6f6f7d3d1e25ef43a5c8
GanXiKang <gan0612xk@hotmail.com>
2023-04-22 03:16:54 +0800

    feat : 編寫Destory

--- Assets/Script/MiniGame1/MG1_DestoryControl.cs
+++ Assets/Script/MiniGame1/MG1_DestoryControl.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class MG1_DestoryControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.tag == "Destory")
+        {
+            Destroy(this.gameObject, 3f);
+        }
     }
 }
