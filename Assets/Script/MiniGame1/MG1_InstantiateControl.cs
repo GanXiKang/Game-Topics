f5e1f3d5c515b496481dd8c7116f92645997a46c
GanXiKang <gan0612xk@hotmail.com>
2023-04-22 02:13:04 +0800

    fix ： 物件的rotation

--- Assets/Script/MiniGame1/MG1_InstantiateControl.cs
+++ Assets/Script/MiniGame1/MG1_InstantiateControl.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class MG1_InstantiateControl : MonoBehaviour
 {
+    public GameObject scissors;
+    public GameObject stone;
+    public GameObject cloth;
+    public Transform playerBrand;
+    public Transform bossBrand;
+
     public GameObject tiger;
     public Transform playerIns;
 
     void Start()
     {
+        playerBrand.transform.Rotate(90, 0, 0);
+        bossBrand.transform.Rotate(90, 0, 0);
         tiger.transform.localScale = new Vector3(2, 2, 2);
         Instantiate(tiger, playerIns.transform.position, playerIns.transform.rotation);
+        Instantiate(stone, playerBrand.transform.position, playerBrand.transform.rotation);
+        Instantiate(cloth, bossBrand.transform.position, bossBrand.transform.rotation);
     }
     void Update()
     {
