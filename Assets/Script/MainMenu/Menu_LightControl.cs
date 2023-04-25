642b6ae024d83f9cadb9b0519b5ee1dcb7ed5b6c
GanXiKang <gan0612xk@hotmail.com>
2023-04-25 23:03:38 +0800

    feat ： 創建新程式

---
+++ Assets/Script/MainMenu/Menu_LightControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_LightControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
