ee18e92643a674aa4950593db9ca29c3650e93b8
GanXiKang <gan0612xk@hotmail.com>
2023-04-26 04:11:11 +0800

    feat ： 調整方向 & speed

--- Assets/Script/MiniGame6/MG6_RelayRotateControl.cs
+++ Assets/Script/MiniGame6/MG6_RelayRotateControl.cs
@@ -6,6 +6,6 @@ public class MG6_RelayRotateControl : MonoBehaviour
 {
     void FixedUpdate()
     {
-        transform.Rotate(0, 15, 0);
+        transform.Rotate(0, -30 * Time.deltaTime, 0);
     }
 }
