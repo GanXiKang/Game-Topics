cfb8f00f3d0b77915a346b0f2f74f61806705404
GanXiKang <gan0612xk@hotmail.com>
2023-04-26 02:43:56 +0800

    feat ： 編寫上面OK和NO的按鈕相關程式碼

--- Assets/Script/MainMenu/Menu_ChoosePlayer.cs
+++ Assets/Script/MainMenu/Menu_ChoosePlayer.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class Menu_ChoosePlayer : MonoBehaviour
 {
     public static float whoPlay, p, chooseP = 1;
-    public static bool confirm,cancel;
+    public static bool confirm, cancel;
 
     public Image[] P1_animals = new Image[4];
     public Image[] P2_animals = new Image[4];
     public Image[] P3_animals = new Image[4];
     public Image[] P4_animals = new Image[4];
+    public Image[] OK = new Image[4];
+    public Image[] NO = new Image[4];
+
     void Update()
     {
         if (chooseP > whoPlay)
@@ -33,19 +36,40 @@ public class Menu_ChoosePlayer : MonoBehaviour
             if (chooseP == 1)
             {
                 chooseP++;
-                confirm =
+                confirm = false;
             }
             if (chooseP == 2)
             {
-
+                chooseP++;
+                confirm = false;
             }
             if (chooseP == 3)
             {
-
+                chooseP++;
+                confirm = false;
             }
             if (chooseP == 4)
             {
-
+                confirm = false;
+            }
+        }
+        if (cancel == true)
+        {
+            if (chooseP == 1)
+            {
+                cancel = false;
+            }
+            if (chooseP == 2)
+            {
+                cancel = false;
+            }
+            if (chooseP == 3)
+            {
+                cancel = false;
+            }
+            if (chooseP == 4)
+            {
+                cancel = false;
             }
         }
     }
