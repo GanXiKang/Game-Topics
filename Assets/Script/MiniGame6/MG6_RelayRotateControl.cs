dd3f1e0a948f287ec0472bfdec4a55c84d249166
GanXiKang <gan0612xk@hotmail.com>
2023-04-26 04:08:19 +0800

    feat ： 編寫物件自轉

--- Assets/Script/MiniGame6/MG6_RelayRotateControl.cs
+++ Assets/Script/MiniGame6/MG6_RelayRotateControl.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MG6_RelayRotateControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.Rotate(0, 15, 0);
     }
 }
