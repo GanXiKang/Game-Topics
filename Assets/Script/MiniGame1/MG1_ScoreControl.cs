e71f9a9664fb0d81f5f70459da744820b059834c
GanXiKang <gan0612xk@hotmail.com>
2023-04-26 23:34:16 +0800

    feat ： 新建立程式碼

---
+++ Assets/Script/MiniGame1/MG1_ScoreControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG1_ScoreControl : MonoBehaviour
+{
+    public GameObject[] aiScore = new GameObject[3];
+    public GameObject[] playerScore = new GameObject[3];
+
+    void Update()
+    {
+        
+    }
+}
