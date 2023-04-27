41c12e71f7bac5d6ed2e077a06c448a109c328f6
GanXiKang <gan0612xk@hotmail.com>
2023-04-28 02:35:53 +0800

    feat ： 編寫1p的全部button控制

--- Assets/Script/MainMenu/Menu_ChoosePlayer.cs
+++ Assets/Script/MainMenu/Menu_ChoosePlayer.cs
@@ -12,9 +12,9 @@ public class Menu_ChoosePlayer : MonoBehaviour
     public GameObject[] P4_animals = new GameObject[4];
     public GameObject[] OK = new GameObject[4];
     public GameObject[] NO = new GameObject[4];
+    public GameObject[] button = new GameObject[8];
 
-    float p;
-    bool confirm;
+    float p, confirm;
 
     void Update()
     {
@@ -29,18 +29,76 @@ public class Menu_ChoosePlayer : MonoBehaviour
     }
     public void OK_1p()
     {
-        confirm = true;
+        confirm++;
+        OK[0].SetActive(false);
+        NO[0].SetActive(true);
+        button[0].SetActive(false);
+        button[1].SetActive(false);
     }
     public void Cancel_1p()
     {
-       cancel = false;
+        confirm--;
+        OK[0].SetActive(true);
+        NO[0].SetActive(false);
+        button[0].SetActive(true);
+        button[1].SetActive(true);
     }
     public void Left_1p()
     {
         p--;
+
+        if (p == 1)
+        {
+            P1_animals[3].SetActive(false);
+            P1_animals[0].SetActive(true);
+            P1_animals[1].SetActive(false);
+        }
+        else if (p == 2)
+        {
+            P1_animals[0].SetActive(false);
+            P1_animals[1].SetActive(true);
+            P1_animals[2].SetActive(false);
+        }
+        else if (p == 3)
+        {
+            P1_animals[1].SetActive(false);
+            P1_animals[2].SetActive(true);
+            P1_animals[3].SetActive(false);
+        }
+        else if (p == 4)
+        {
+            P1_animals[2].SetActive(false);
+            P1_animals[3].SetActive(true);
+            P1_animals[0].SetActive(false);
+        }
     }
     public void B_Right_1p()
     {
        p++;
+
+        if (p == 1)
+        {
+            P1_animals[3].SetActive(false);
+            P1_animals[0].SetActive(true);
+            P1_animals[1].SetActive(false);
+        }
+        else if (p == 2)
+        {
+            P1_animals[0].SetActive(false);
+            P1_animals[1].SetActive(true);
+            P1_animals[2].SetActive(false);
+        }
+        else if (p == 3)
+        {
+            P1_animals[1].SetActive(false);
+            P1_animals[2].SetActive(true);
+            P1_animals[3].SetActive(false);
+        }
+        else if (p == 4)
+        {
+            P1_animals[2].SetActive(false);
+            P1_animals[3].SetActive(true);
+            P1_animals[0].SetActive(false);
+        }
     }
 }
