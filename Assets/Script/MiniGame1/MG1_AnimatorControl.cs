5b1d38147f8cc3906207d823aca549c46b9145d7
GanXiKang <gan0612xk@hotmail.com>
2023-04-29 23:42:54 +0800

    feat ： 新建程式

---
+++ Assets/Script/MiniGame1/MG1_AnimatorControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG1_AnimatorControl : MonoBehaviour
+{
+    Animator ani;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        
+    }
+}
