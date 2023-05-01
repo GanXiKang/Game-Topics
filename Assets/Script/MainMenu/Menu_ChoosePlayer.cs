9d0119852e6fac112253e2f2edc014519006ac53
GanXiKang <gan0612xk@hotmail.com>
2023-05-01 16:19:04 +0800

    feat ： 更改前往關卡的序號

--- Assets/Script/MainMenu/Menu_ChoosePlayer.cs
+++ Assets/Script/MainMenu/Menu_ChoosePlayer.cs
@@ -50,7 +50,7 @@ public class Menu_ChoosePlayer : MonoBehaviour
         NO[3].SetActive(false);
         yield return new WaitForSeconds(2f);
         InstantiatePointControl.isInstantiate = true;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(7);
     }
     void Limit()
     {
