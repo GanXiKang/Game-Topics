7234a20ee8d1cd424ece312e644111a07dbf3747
GanXiKang <gan0612xk@hotmail.com>
2023-05-02 23:29:50 +0800

    feat ： 編寫teach程式碼

---
+++ Assets/Script/MiniGame5/MG5_TeachingControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG5_TeachingControl : MonoBehaviour
+{
+    public GameObject teach, mouse1, mouse2, mouse3;
+    void Start()
+    {
+        StartCoroutine(CloseTeach());
+    }
+    void Update()
+    {
+        if (MG2_StartButtonControl.isStart)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                teach.SetActive(false);
+            }
+        }
+    }
+    IEnumerator CloseTeach()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        mouse1.SetActive(false);
+        mouse2.SetActive(true);
+        mouse3.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        mouse1.SetActive(false);
+        mouse2.SetActive(false);
+        mouse3.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        mouse1.SetActive(true);
+        mouse2.SetActive(false);
+        mouse3.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        mouse1.SetActive(false);
+        mouse2.SetActive(true);
+        mouse3.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        mouse1.SetActive(false);
+        mouse2.SetActive(false);
+        mouse3.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        teach.SetActive(false);
+    }
+}
