15a156f952ef3e12d28e973173a427f009852965
GanXiKang <gan0612xk@hotmail.com>
2023-05-03 06:10:50 +0800

    feat ： 竟然不要public 需要拉物件的事寫在perfab

--- Assets/Script/MiniGame1/MG1_AnimatorControl.cs
+++ Assets/Script/MiniGame1/MG1_AnimatorControl.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MG1_AnimatorControl : MonoBehaviour
 {
     Animator ani;
-    public AudioSource BGM;
-    public AudioClip win, lose;
     void Start()
     {
         ani = GetComponent<Animator>();
@@ -16,12 +14,10 @@ public class MG1_AnimatorControl : MonoBehaviour
         if (MG1_GameControl.playerAniWin)
         {
             ani.SetBool("Win", true);
-            //BGM.PlayOneShot(win);
         }
         if (MG1_GameControl.playerAniLose)
         {
             ani.SetBool("Lose", true);
-            //BGM.PlayOneShot(lose);
         }
     }
 }
