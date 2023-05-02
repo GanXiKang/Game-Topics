35ce3177933ed9fc62826a89d50bebbf34c1bb53
GanXiKang <gan0612xk@hotmail.com>
2023-05-03 07:42:52 +0800

    feat ： 編寫音效

--- Assets/Script/MiniGame2/MG2_StartButtonControl.cs
+++ Assets/Script/MiniGame2/MG2_StartButtonControl.cs
@@ -7,9 +7,13 @@ public class MG2_StartButtonControl : MonoBehaviour
     public GameObject startUI;
     public static bool isStart = false;
 
+    public AudioSource BGM;
+    public AudioClip button;
+
     public void Button_Start()
     {
         startUI.SetActive(false);
         isStart = true;
+        BGM.PlayOneShot(button);
     }
 }
