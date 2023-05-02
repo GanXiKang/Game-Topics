0bd97f133360f9fe2324031e83ebe68d1a283c95
GanXiKang <gan0612xk@hotmail.com>
2023-05-03 05:37:24 +0800

    feat ： 同下

--- Assets/Script/MainMenu/Menu_ButtonControl.cs
+++ Assets/Script/MainMenu/Menu_ButtonControl.cs
@@ -9,23 +9,30 @@ public class Menu_ButtonControl : MonoBehaviour
     public GameObject menu3;
     public GameObject menu_t;
 
+    public AudioSource BGM;
+    public AudioClip onClick;
+
     public void B_Start()
     {
         menu1.SetActive(false);
         menu2.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
     public void B_Tutorial()
     {
         menu1.SetActive(false);
         menu_t.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
     public void B_Setting()
     {
         menu1.SetActive(false);
         menu_t.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
     public void B_Exit()
     {
+        BGM.PlayOneShot(onClick);
         Application.Quit();
     }
     public void B_Close()
@@ -33,23 +40,27 @@ public class Menu_ButtonControl : MonoBehaviour
         menu1.SetActive(true);
         menu2.SetActive(false);
         menu_t.SetActive(false);
+        BGM.PlayOneShot(onClick);
     }
     public void B_2p()
     {
         Menu_ChoosePlayer.whoPlay = 2;
         menu2.SetActive(false);
         menu3.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
     public void B_3p()
     {
         Menu_ChoosePlayer.whoPlay = 3;
         menu2.SetActive(false);
         menu3.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
     public void B_4p()
     {
         Menu_ChoosePlayer.whoPlay = 4;
         menu2.SetActive(false);
         menu3.SetActive(true);
+        BGM.PlayOneShot(onClick);
     }
 }
