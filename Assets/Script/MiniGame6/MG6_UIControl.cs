11ebe12a4ce07dc2939906a3564f37a1cb7bba63
GanXiKang <gan0612xk@hotmail.com>
2023-05-03 17:15:25 +0800

    feat ： 添加start button 的音效

--- Assets/Script/MiniGame6/MG6_UIControl.cs
+++ Assets/Script/MiniGame6/MG6_UIControl.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MG6_UIControl : MonoBehaviour
 {
     public GameObject startUI;
+    public AudioSource BGM;
+    public AudioClip button;
     public static bool isStart = false;
+    
 
     public void Button_Start()
     {
         startUI.SetActive(false);
         isStart = true;
+        BGM.PlayOneShot(button);
     }
 }
