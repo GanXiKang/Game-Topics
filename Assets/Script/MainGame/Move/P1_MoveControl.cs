a067f3a5facbc5f75802a368416b36e3a8d56c45
GanXiKang <gan0612xk@hotmail.com>
2023-05-04 21:38:42 +0800

    feat ： 移除DoMove

--- Assets/Script/MainGame/Move/P1_MoveControl.cs
+++ Assets/Script/MainGame/Move/P1_MoveControl.cs
@@ -1,71 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+using UnityEngine.AI;
+
 
 public class P1_MoveControl : MonoBehaviour
 {
-    GameObject[] p = new GameObject[8];
+    NavMeshAgent agent;
+
+    GameObject[] p = new GameObject[11];
 
     public static bool Award;                    //false : Wave , True : Jump
 
     void Start()
     {
-        DOTween.SetTweensCapacity(500, 125);
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        p[1] = GameObject.Find("Point1");
+        p[2] = GameObject.Find("Point2");
+        p[3] = GameObject.Find("Point3");
+        p[4] = GameObject.Find("Point4");
+        p[5] = GameObject.Find("Point5");
+        p[6] = GameObject.Find("Point6");
+        p[7] = GameObject.Find("Point7");
+        p[8] = GameObject.Find("Point8");
+        p[9] = GameObject.Find("Point9");
+        p[10] = GameObject.Find("Point10");
     }
     void Update()
     {
         MovePoint();                        //ÎÄ×Ö÷»×ÓÓÃ
-        p[1] = GameObject.FindGameObjectWithTag("Point1");
-        p[2] = GameObject.FindGameObjectWithTag("Point2");
-        p[3] = GameObject.FindGameObjectWithTag("Point3");
-        p[4] = GameObject.FindGameObjectWithTag("Point4");
-        p[5] = GameObject.FindGameObjectWithTag("Point5");
-        p[6] = GameObject.FindGameObjectWithTag("Point6");
-        p[7] = GameObject.FindGameObjectWithTag("Point7");
+        
 
     }
     void MovePoint()
     {
-        if (Dice.P1_totalNum == 0)
-        {
-            //transform.DOMove(p[0].transform.position, 1);
-        }
-        if (Dice.P1_totalNum == 1)
-        {
-            transform.DOMove(p[1].transform.position, 3);
-            Award = false;
-        }
-        if (Dice.P1_totalNum == 2)
-        {
-            transform.DOMove(p[2].transform.position, 3);
-            Award = true;
-        }
-        if (Dice.P1_totalNum == 3)
-        {
-            transform.DOMove(p[3].transform.position, 3);
-            Award = false;
-        }
-        if (Dice.P1_totalNum == 4)
-        {
-            transform.DOMove(p[4].transform.position, 3);
-            Award = false;
-        }
-        if (Dice.P1_totalNum == 5)
-        {
-            transform.DOMove(p[5].transform.position, 3);
-            Award = true;
-        }
-        if (Dice.P1_totalNum == 6)
-        {
-            transform.DOMove(p[6].transform.position, 3);
-            Award = false;
-        }
-        if (Dice.P1_totalNum > 6)
-        {
-            transform.DOMove(p[7].transform.position, 5);
-            Award = false;
-        }
+        
     }
 }
