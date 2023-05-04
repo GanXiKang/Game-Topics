9653aa8805428dd35f9a1df71988d7be4cf02108
GanXiKang <gan0612xk@hotmail.com>
2023-05-05 02:35:25 +0800

    feat ： 優化

--- Assets/Script/MainGame/Chest/ChestColliderControl.cs
+++ Assets/Script/MainGame/Chest/ChestColliderControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChestColliderControl : MonoBehaviour
 {
-    int a;
+    int a, box1Point = 2, box2Point = 6;
     public static bool isOpen1, isOpen2;
 
     void Award()
@@ -38,13 +38,13 @@ public class ChestColliderControl : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "P1" || other.tag == "P2" || other.tag == "P3" || other.tag == "P4")
-        {
-            if (Dice.P1_totalNum == 4 || Dice.P2_totalNum == 4 || Dice.P3_totalNum == 4 || Dice.P4_totalNum == 4)
+        { 
+            if (Dice.P1_totalNum == box1Point || Dice.P2_totalNum == box1Point || Dice.P3_totalNum == box1Point || Dice.P4_totalNum == box1Point)
             {
                 isOpen1 = true;
                 Award();
             }
-            if (Dice.P1_totalNum == 0 || Dice.P2_totalNum == 0 || Dice.P3_totalNum == 0 || Dice.P4_totalNum == 0)
+            if (Dice.P1_totalNum == box2Point || Dice.P2_totalNum == box2Point || Dice.P3_totalNum == box2Point || Dice.P4_totalNum == box2Point)
             {
                 isOpen2 = true;
                 Award();
