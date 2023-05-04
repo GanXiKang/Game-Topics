02330c9d3c3c10b28bf90e514f887e6e0164d95f
GanXiKang <gan0612xk@hotmail.com>
2023-05-05 01:33:45 +0800

    fix : 避免碰到也會進入小游戲

--- Assets/Script/MainGame/MiniGame/MiniGameColliderControl.cs
+++ Assets/Script/MainGame/MiniGame/MiniGameColliderControl.cs
@@ -5,36 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MiniGameColliderControl : MonoBehaviour
 {
-    public static bool P1_playGame = true, P2_playGame = true, P3_playGame = true, P4_playGame = true;
+   public static bool P1_playGame = true, P2_playGame = true, P3_playGame = true, P4_playGame = true, isMiniGamePoint = false;
     public static float p;
 
-    public int miniGame;
+    public int miniGame, pointN;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "P1" && P1_playGame == true)
+        if (Dice.P1_totalNum == pointN)
         {
-            p = 1;
-            StartCoroutine(StartMiniGame());
-        }
-        if (other.tag == "P2" && P2_playGame == true)
-        {
-            p = 2;
-            StartCoroutine(StartMiniGame());
-        }
-        if (other.tag == "P3" && P3_playGame == true)
-        {
-            p = 3;
-            StartCoroutine(StartMiniGame());
-        }
-        if (other.tag == "P4" && P4_playGame == true)
-        {
-            p = 4;
-            StartCoroutine(StartMiniGame());
+            if (other.tag == "P1" && P1_playGame)
+            {
+                p = 1;
+                StartCoroutine(StartMiniGame());
+            }
+            if (other.tag == "P2" && P2_playGame)
+            {
+                p = 2;
+                StartCoroutine(StartMiniGame());
+            }
+            if (other.tag == "P3" && P3_playGame)
+            {
+                p = 3;
+                StartCoroutine(StartMiniGame());
+            }
+            if (other.tag == "P4" && P4_playGame)
+            {
+                p = 4;
+                StartCoroutine(StartMiniGame());
+            }
         }
     }
     private void OnTriggerExit(Collider other)
     {
+        isMiniGamePoint = false;
+
         if (other.tag == "P1")
         {
             P1_playGame = true;
