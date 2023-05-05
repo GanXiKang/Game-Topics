6b5434464974b985ce64ecbaa9afdc7596409114
GanXiKang <gan0612xk@hotmail.com>
2023-05-05 22:45:56 +0800

    feat ： 編寫give tag

--- Assets/Script/MainGame/Move/GiveTagControl.cs
+++ Assets/Script/MainGame/Move/GiveTagControl.cs
@@ -6,6 +6,84 @@ public class GiveTagControl : MonoBehaviour
 {
     void Update()
     {
-        gameObject.tag = "P1";
+        if (gameObject.tag == "Mouse")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 1)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 1)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 1)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 1)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        if (gameObject.tag == "Cow")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 2)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 2)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 2)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 2)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        if (gameObject.tag == "Tiger")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 3)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 3)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 3)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 3)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        if (gameObject.tag == "Rabbit")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 4)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 4)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 4)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 4)
+            {
+                gameObject.tag = "P4";
+            }
+        }
     }
 }
