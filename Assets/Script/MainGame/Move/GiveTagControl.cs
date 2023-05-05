6de2d1607be60685dbc778fa141d792e85950e50
GanXiKang <gan0612xk@hotmail.com>
2023-05-05 22:39:13 +0800

    feat : 編寫給tag程式碼

--- Assets/Script/MainGame/Move/GiveTagControl.cs
+++ Assets/Script/MainGame/Move/GiveTagControl.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GiveTagControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        gameObject.tag = "P1";
     }
 }
