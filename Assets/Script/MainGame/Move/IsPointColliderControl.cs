015fd2f8dd2b711a13d00c8e0471a12961c3082a
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 18:40:34 +0800

    feat ： 内容大改

--- Assets/Script/MainGame/Move/IsPointColliderControl.cs
+++ Assets/Script/MainGame/Move/IsPointColliderControl.cs
@@ -6,20 +6,34 @@ using UnityEngine.UI;
 public class IsPointColliderControl : MonoBehaviour
 {
     GameObject Interactive_UI;
+    public int pointNum;
 
     void Start()
     {
         Interactive_UI = GameObject.Find("InteractiveUI");
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Point")
+        if (other.tag == "P1" && Dice.P1_totalNum == pointNum)
+        {
+            StartCoroutine(NextPlayer());
+        }
+        if (other.tag == "P2" && Dice.P2_totalNum == pointNum)
+        {
+            StartCoroutine(NextPlayer());
+        }
+        if (other.tag == "P3" && Dice.P3_totalNum == pointNum)
+        {
+            StartCoroutine(NextPlayer());
+        }
+        if (other.tag == "P4" && Dice.P4_totalNum == pointNum)
         {
             StartCoroutine(NextPlayer());
         }
     }
     IEnumerator NextPlayer()
     {
+        print("yes");
         yield return new WaitForSeconds(3f);
         Interactive_UI.SetActive(true);
         CameraControl.changeCamera++;
