92131b5a7cd1e00a398f67f140aa6ddb79d7897a
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 21:46:18 +0800

    feat ： 需計算的程式移出來

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -4,28 +4,9 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public static int changeCameraNum;
-
-    void Start()
-    {
-        changeCameraNum = 1;
-    }
     void Update()
     {
-        print("changeCameraNum" + changeCameraNum);
-        if (changeCameraNum > Menu_ChoosePlayer.whoPlay)
-        {
-            changeCameraNum = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            changeCameraNum++;
-        }
-        ChangeCamera();
-    }
-    void ChangeCamera()
-    {
-        switch (changeCameraNum)
+        switch (ChangeCameraControl.changeCameraNum)
         {
             case 1:
                 if (gameObject.tag == "P2Camera" || gameObject.tag == "P3Camera" || gameObject.tag == "P4Camera")
