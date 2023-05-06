4997cf96ba8f4cdd88c306ca75aff2be2b06337a
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 23:14:17 +0800

    feat ： delete程式

--- Assets/Script/MainGame/Collider/IsPointColliderControl.cs
+++ Assets/Script/MainGame/Collider/IsPointColliderControl.cs
@@ -4,33 +4,6 @@ using UnityEngine;
 
 public class IsPointColliderControl : MonoBehaviour
 {
-    public GameObject Interactive_UI;
-    public int pointNum;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "P1" && Dice.P1_totalNum == pointNum)
-        {
-            StartCoroutine(NextPlayer());
-        }
-        if (other.tag == "P2" && Dice.P2_totalNum == pointNum)
-        {
-            StartCoroutine(NextPlayer());
-        }
-        if (other.tag == "P3" && Dice.P3_totalNum == pointNum)
-        {
-            StartCoroutine(NextPlayer());
-        }
-        if (other.tag == "P4" && Dice.P4_totalNum == pointNum)
-        {
-            StartCoroutine(NextPlayer());
-        }
-    }
-    IEnumerator NextPlayer()
-    {
-        yield return new WaitForSeconds(3f);
-        Interactive_UI.SetActive(true);
-        ChangeCameraControl.changeCameraNum++;
-    }
 }
 
