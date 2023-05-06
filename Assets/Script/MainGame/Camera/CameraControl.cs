2721ba6447bd1bdba38e87d79793495e43c101a6
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 19:11:27 +0800

    feat ： 修改程式碼

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -8,31 +8,29 @@ public class CameraControl : MonoBehaviour
 
     GameObject[] PlayerCamera = new GameObject[5];
 
-    bool closeCamera = false, findCamera = true;
+    bool closeCamera = true, findCamera = true;
 
     void Update()
     {
-        OpeningSet();
+        if (findCamera)
+        {
+            OpeningSet();
+        }
         ChangeCamera();
     }
     void OpeningSet()
     {
-        if (findCamera)
-        {
-            PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
-            PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
-            PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
-            PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
-            findCamera = false;
-            closeCamera = true;
-        }
+        PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
+        PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
+        PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
+        PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
+
         if (closeCamera)
         {
-            if (gameObject.tag == "P2Camera" || gameObject.tag == "P3Camera" || gameObject.tag == "P4Camera")
-            {
-                gameObject.SetActive(false);
-                closeCamera = false;
-            }
+            PlayerCamera[2].SetActive(false);
+            PlayerCamera[3].SetActive(false);
+            PlayerCamera[4].SetActive(false);
+            closeCamera = false;
         }
     }
     void ChangeCamera()
