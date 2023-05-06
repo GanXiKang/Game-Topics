61bb4fa76550840af47c1d4c089fc85fe27d09fc
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 16:47:43 +0800

    feat ： 將程式碼cut過去

--- Assets/Script/MainGame/Camera/GiveTagCameraControl.cs
+++ Assets/Script/MainGame/Camera/GiveTagCameraControl.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class GiveTagCameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.tag == "Mouse")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 1)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 1)
+            {
+                gameObject.tag = "P2Camrea";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 1)
+            {
+                gameObject.tag = "P3Camrea";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 1)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        if (gameObject.tag == "Cow")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 2)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 2)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 2)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 2)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        if (gameObject.tag == "Tiger")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 3)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 3)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 3)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 3)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        if (gameObject.tag == "Rabbit")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 4)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 4)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 4)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 4)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
     }
 }
