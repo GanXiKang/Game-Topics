0ab30a9cf67215f7037d9ad59f51e673593fbb4b
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 20:10:12 +0800

    feat ： 把Find gameobject移過去

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    bool closeCamera = true;
+    public static GameObject[] PlayerCamera = new GameObject[5];
+
+    bool closeCamera = true, findCamera = true;
     void Update()
     {
         if (closeCamera)
@@ -12,6 +14,20 @@ public class CameraControl : MonoBehaviour
             if (gameObject.tag == "P2Camera" || gameObject.tag == "P3Camera" || gameObject.tag == "P4Camera")
             {
                 this.gameObject.SetActive(false);
+            }
+
+            if (findCamera)
+            {
+                PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
+                PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
+                PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
+                PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
+
+                print(PlayerCamera[1].tag);
+                print(PlayerCamera[2].tag);
+                print(PlayerCamera[3].tag);
+                print(PlayerCamera[4].tag);
+                findCamera = false;
             }
             closeCamera = false;
         }
