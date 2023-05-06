8f8b38d77882918c13a1821fd94f5f55fcc8759b
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 20:18:25 +0800

    feat : delete //

--- Assets/Script/MainGame/Camera/ChangeCameraControl.cs
+++ Assets/Script/MainGame/Camera/ChangeCameraControl.cs
@@ -7,67 +7,52 @@ public class ChangeCameraControl : MonoBehaviour
     
     public static int changeCamera = 1;
 
-    //bool findCamera;
 
-    //void Update()
-    //{
-    //    //if (findCamera)
-    //    //{
-    //    //PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
-    //    //PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
-    //    //PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
-    //    //PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
-    //    //    findCamera = false;
-    //    //}
-    //    if (Input.GetKeyDown(KeyCode.P))
-    //    {
-    //        print(PlayerCamera[1]);
-    //        print(PlayerCamera[2]);
-    //        print(PlayerCamera[3]);
-    //        print(PlayerCamera[4]);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.C))
-    //    {
-    //        changeCamera++;
-    //    }
-    //    ChangeCamera();
-    //}
-    //void ChangeCamera()
-    //{
-    //    if (changeCamera > Menu_ChoosePlayer.whoPlay)
-    //    {
-    //        changeCamera = 1;
-    //    }
+    void Update()
+    {
 
-    //    switch (changeCamera)
-    //    {
-    //        case 1:
-    //            PlayerCamera[1].SetActive(true);
-    //            PlayerCamera[2].SetActive(false);
-    //            PlayerCamera[3].SetActive(false);
-    //            PlayerCamera[4].SetActive(false);
-    //            break;
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            changeCamera++;
+        }
+        ChangeCamera();
+    }
+    void ChangeCamera()
+    {
+        if (changeCamera > Menu_ChoosePlayer.whoPlay)
+        {
+            changeCamera = 1;
+        }
 
-    //        case 2:
-    //            PlayerCamera[1].SetActive(false);
-    //            PlayerCamera[2].SetActive(true);
-    //            PlayerCamera[3].SetActive(false);
-    //            PlayerCamera[4].SetActive(false);
-    //            break;
+        switch (changeCamera)
+        {
+            case 1:
+                CameraControl.PlayerCamera[1].SetActive(true);
+                CameraControl.PlayerCamera[2].SetActive(false);
+                CameraControl.PlayerCamera[3].SetActive(false);
+                CameraControl.PlayerCamera[4].SetActive(false);
+                break;
 
-    //        case 3:
-    //            PlayerCamera[1].SetActive(false);
-    //            PlayerCamera[2].SetActive(false);
-    //            PlayerCamera[3].SetActive(true);
-    //            PlayerCamera[4].SetActive(false);
-    //            break;
+            case 2:
+                CameraControl.PlayerCamera[1].SetActive(false);
+                CameraControl.PlayerCamera[2].SetActive(true);
+                CameraControl.PlayerCamera[3].SetActive(false);
+                CameraControl.PlayerCamera[4].SetActive(false);
+                break;
 
-    //        case 4:
-    //            PlayerCamera[1].SetActive(false);
-    //            PlayerCamera[2].SetActive(false);
-    //            PlayerCamera[3].SetActive(false);
-    //            PlayerCamera[4].SetActive(true);
-    //            break;
-    //    }
-    //}
+            case 3:
+                CameraControl.PlayerCamera[1].SetActive(false);
+                CameraControl.PlayerCamera[2].SetActive(false);
+                CameraControl.PlayerCamera[3].SetActive(true);
+                CameraControl.PlayerCamera[4].SetActive(false);
+                break;
+
+            case 4:
+                CameraControl.PlayerCamera[1].SetActive(false);
+                CameraControl.PlayerCamera[2].SetActive(false);
+                CameraControl.PlayerCamera[3].SetActive(false);
+                CameraControl.PlayerCamera[4].SetActive(true);
+                break;
+        }
+    }
 }
