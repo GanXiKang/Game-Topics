17191e7dda4a4f07c0f5124cad63942f58aaa658
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 17:37:50 +0800

    fix ： 先find在Close

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -6,29 +6,32 @@ public class CameraControl : MonoBehaviour
 {
     GameObject[] PlayerCamera = new GameObject[5];
 
-    bool closeCamera = true, findCamera = false;
+    bool closeCamera = false, findCamera = true;
 
     void Update()
     {
+        if (findCamera)
+        {
+            PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
+            PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
+            PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
+            PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
+            findCamera = false;
+            closeCamera = true;
+        }
         if (closeCamera)
         {
             if (gameObject.tag == "P2Camera"|| gameObject.tag == "P3Camera"|| gameObject.tag == "P4Camera")
             {
                 gameObject.SetActive(false);
                 closeCamera = false;
-                //findCamera = true;
             }
         }
 
-        //if (findCamera)
-        //{
-            PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
-            PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
-            PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
-            PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
-        //    findCamera = false;
-        //}
         print(PlayerCamera[1].tag);
+        print(PlayerCamera[2].tag);
+        print(PlayerCamera[3].tag);
+        print(PlayerCamera[4].tag);
         if (Input.GetKeyDown(KeyCode.C))
         {
             PlayerCamera[1].SetActive(false);
