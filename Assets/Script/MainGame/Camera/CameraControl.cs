328f0c663894f18d147b21ef6c1f560f0f715d13
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 17:33:13 +0800

    feat ： 找到BUG

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -6,7 +6,7 @@ public class CameraControl : MonoBehaviour
 {
     GameObject[] PlayerCamera = new GameObject[5];
 
-    bool closeCamera = true;
+    bool closeCamera = true, findCamera = false;
 
     void Update()
     {
@@ -16,7 +16,23 @@ public class CameraControl : MonoBehaviour
             {
                 gameObject.SetActive(false);
                 closeCamera = false;
+                //findCamera = true;
             }
+        }
+
+        //if (findCamera)
+        //{
+            PlayerCamera[1] = GameObject.FindGameObjectWithTag("P1Camera");
+            PlayerCamera[2] = GameObject.FindGameObjectWithTag("P2Camera");
+            PlayerCamera[3] = GameObject.FindGameObjectWithTag("P3Camera");
+            PlayerCamera[4] = GameObject.FindGameObjectWithTag("P4Camera");
+        //    findCamera = false;
+        //}
+        print(PlayerCamera[1].tag);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            PlayerCamera[1].SetActive(false);
+            PlayerCamera[2].SetActive(true);
         }
     }
 }
