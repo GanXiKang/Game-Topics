e3e0a39b9b507dba4b2f1504a7ae6fdbbc612b15
GanXiKang <gan0612xk@hotmail.com>
2023-05-06 17:15:13 +0800

    feat ： 編寫測試切換camera

--- Assets/Script/MainGame/Camera/CameraControl.cs
+++ Assets/Script/MainGame/Camera/CameraControl.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    void Start()
+    bool closeCamera = true;
+    void Update()
     {
+        if (gameObject.tag == "P2Camera" && closeCamera || gameObject.tag == "P3Camera" && closeCamera || gameObject.tag == "P4Camera" && closeCamera)
+        {
+            gameObject.SetActive(false);
+            closeCamera = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
             if (gameObject.tag == "P2Camera")
+            {
+                gameObject.SetActive(true);
+            }
+            if (gameObject.tag == "P1Camera")
             {
                 gameObject.SetActive(false);
             }
-
+        }
     }
 }
