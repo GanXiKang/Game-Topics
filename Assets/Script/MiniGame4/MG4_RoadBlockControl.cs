2a2267cdd7a98ebea78a2e68c856c3b7b58ac6a5
GanXiKang <gan0612xk@hotmail.com>
2023-05-08 00:41:50 +0800

    feat : destory時間

--- Assets/Script/MiniGame4/MG4_RoadBlockControl.cs
+++ Assets/Script/MiniGame4/MG4_RoadBlockControl.cs
@@ -13,7 +13,7 @@ public class MG4_RoadBlockControl : MonoBehaviour
         {
             combo = 0;
             hit = true;
-            Destroy(other.gameObject);
+            Destroy(other.gameObject, 0.3f);
         }
         if (other.tag == "RecordJump")
         {
