0a0e1a4f3492ba5cbf94eac55e51f7c987e07242
GanXiKang <gan0612xk@hotmail.com>
2023-05-08 02:01:58 +0800

    feat ： 先編寫一個測試

--- Assets/Script/MainGame/Collider/ChestColliderControl.cs
+++ Assets/Script/MainGame/Collider/ChestColliderControl.cs
@@ -48,7 +48,8 @@ public class ChestColliderControl : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "P1" || other.tag == "P2" || other.tag == "P3" || other.tag == "P4")
-        { 
+        {
+            AnimatorControl.isMove = false;
             if (Dice.P1_totalNum == boxPoint[1] || Dice.P2_totalNum == boxPoint[1] || Dice.P3_totalNum == boxPoint[1] || Dice.P4_totalNum == boxPoint[1])
             {
                 isOpen1 = true;
