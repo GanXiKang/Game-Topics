b82e431917936d9d958b1955063fcf2be0cce346
GanXiKang <gan0612xk@hotmail.com>
2023-05-07 14:49:39 +0800

    feat ： 編寫特效

---
+++ Assets/Script/MiniGame2/MG2_GameEffectsControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG2_GameEffectsControl : MonoBehaviour
+{
+    public GameObject effects;
+
+    void FixedUpdate()
+    {
+        if (MG2_GameResultControl.win)
+        {
+            Instantiate(effects, transform.position, transform.rotation);
+        }
+    }
+}
