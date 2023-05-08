71fe2cff24f8b6bfb260e8b10568fe50685992fe
GanXiKang <gan0612xk@hotmail.com>
2023-05-09 00:10:31 +0800

    feat ： 優化程式碼

--- Assets/Script/MainGame/Collider/ChestColliderControl.cs
+++ Assets/Script/MainGame/Collider/ChestColliderControl.cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class ChestColliderControl : MonoBehaviour
 {
-    int[] boxPoint = new int[3];
+    public int boxPoint, isOpenNumber;
 
     public static bool isOpen1, isOpen2;
 
-    void Start()
+    void OpenBoxJudge()
     {
-        boxPoint[1] = 2;
-        boxPoint[2] = 6;        
+        if (isOpenNumber == 1)
+        {
+            isOpen1 = true;
+        }
+        else if (isOpenNumber == 2)
+        {
+            isOpen2 = true;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "P1" || other.tag == "P2" || other.tag == "P3" || other.tag == "P4")
         {
-            if (Dice.P1_totalNum == boxPoint[1] || Dice.P2_totalNum == boxPoint[1] || Dice.P3_totalNum == boxPoint[1] || Dice.P4_totalNum == boxPoint[1])
+            if (Dice.P1_totalNum == boxPoint || Dice.P2_totalNum == boxPoint || Dice.P3_totalNum == boxPoint || Dice.P4_totalNum == boxPoint)
             {
                 StartCoroutine(ChangeCamera());
-                isOpen1 = true;
-            }
-            if (Dice.P1_totalNum == boxPoint[2] || Dice.P2_totalNum == boxPoint[2] || Dice.P3_totalNum == boxPoint[2] || Dice.P4_totalNum == boxPoint[2])
-            {
-                StartCoroutine(ChangeCamera());
-                isOpen2 = true;
             }
         }
     }
     IEnumerator ChangeCamera()
     {
+        OpenBoxJudge();
         AnimatorControl.isMove = false;
         yield return new WaitForSeconds(2f);
         ChangeCameraControl.changeCameraNum++;
