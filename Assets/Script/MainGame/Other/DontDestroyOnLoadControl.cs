072a850b062aa854d271bcde423aabb70108361f
GanXiKang <gan0612xk@hotmail.com>
2023-05-09 01:57:17 +0800

    feat ： 參考 網上

--- Assets/Script/MainGame/Other/DontDestroyOnLoadControl.cs
+++ Assets/Script/MainGame/Other/DontDestroyOnLoadControl.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DontDestroyOnLoadControl : MonoBehaviour
 {
+    static DontDestroyOnLoadControl instance;
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (this != instance)
+        {
+            Destroy(gameObject);
+        }
     }
 }
