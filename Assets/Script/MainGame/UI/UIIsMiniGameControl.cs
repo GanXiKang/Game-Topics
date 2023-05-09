21de5a76f68eb418f39bf4abc03ff16cc868277b
GanXiKang <gan0612xk@hotmail.com>
2023-05-09 22:56:43 +0800

    feat ： 編寫音效控制

--- Assets/Script/MainGame/UI/UIIsMiniGameControl.cs
+++ Assets/Script/MainGame/UI/UIIsMiniGameControl.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class UIIsMiniGameControl : MonoBehaviour
 {
+    AudioSource BGM;
     public GameObject UI_MainGame;
-    
+    void Start()
+    {
+        BGM = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (MiniGameColliderControl.isMiniGame)
         {
             UI_MainGame.SetActive(false);
+            BGM.Stop();
         }
         else
         {
             UI_MainGame.SetActive(true);
+            BGM.Play();
         }
     }
 }
