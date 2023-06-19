e9e7f824437b611dbdb862723768731d12bc1e93
GanXiKang <gan0612xk@hotmail.com>
2023-06-19 23:10:12 +0800

    feat ： 編寫重新生成

--- Assets/Script/MainMenu/Menu_ChoosePlayer.cs
+++ Assets/Script/MainMenu/Menu_ChoosePlayer.cs
@@ -82,6 +82,7 @@ public class Menu_ChoosePlayer : MonoBehaviour
         NO[2].SetActive(false);
         NO[3].SetActive(false);
         yield return new WaitForSeconds(1f);
+        InstantiatePointControl.isStart = true;
         SceneManager.LoadScene(7);
     }
     void Limit()
