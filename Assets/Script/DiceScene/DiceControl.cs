acc2ea9698c7daeced5fce95b3650c32b709682e
GanXiKang <gan0612xk@hotmail.com>
2023-07-03 23:41:17 +0800

    feat ： 嘗試采用Quaternion.Lerp

--- Assets/Script/DiceScene/DiceControl.cs
+++ Assets/Script/DiceScene/DiceControl.cs
@@ -9,6 +9,9 @@ public class DiceControl : MonoBehaviour
     public float rotationForce;
     public Vector3 rotationForce3;
 
+    Quaternion stopQ;
+    Quaternion[] q = new Quaternion[7];
+
     int randomNum;
     bool isRoll, result;
     void Start()
@@ -50,29 +53,39 @@ public class DiceControl : MonoBehaviour
     void DiceRandomDice()
     {
         result = false;
+        stopQ = Quaternion.identity;
         rb.angularVelocity = Vector3.zero;
         randomNum = Random.Range(1, 7);
+        print(randomNum);
         switch (randomNum)
         {
             case 1:
-                transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[1], 2f);
                 break;
             case 2:
-                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[2], 2f);
                 break;
             case 3:
-                transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[3], 2f);
                 break;
             case 4:
-                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[4], 2f);
                 break;
             case 5:
-                transform.rotation = Quaternion.Euler(270f, 0f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[5], 2f);
                 break;
             case 6:
-                transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                transform.rotation = Quaternion.Lerp(stopQ, q[6], 2f);
                 break;
-
         }
+    }
+    void FindQuaternionEuler()
+    {
+        q[1] = transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+        q[2] = transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        q[3] = transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        q[4] = transform.rotation = Quaternion.Euler(270f, 0f, 0f);
+        q[5] = transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        q[6] = transform.rotation = Quaternion.Euler(0f, 900f, 0f);
     }
 }
