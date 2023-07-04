09c52b157dec35d861b97be604600ef49f1c0ae2
GanXiKang <gan0612xk@hotmail.com>
2023-07-04 22:22:19 +0800

    feat ： 編寫gameobject存取角色

--- Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
+++ Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class DiceInstantionAnimalsControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] animals = new GameObject[4];
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
