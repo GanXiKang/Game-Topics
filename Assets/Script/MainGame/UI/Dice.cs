78befd02956a6149e485cd1c7b37da40435599ff
GanXiKang <gan0612xk@hotmail.com>
2023-07-05 20:12:16 +0800

    feat ： 添加一個bool

--- Assets/Script/MainGame/UI/Dice.cs
+++ Assets/Script/MainGame/UI/Dice.cs
@@ -7,7 +7,7 @@ public class Dice : MonoBehaviour
 {
     int diceNum = 0, who;
     public static int P1_totalNum = 0, P2_totalNum = 0, P3_totalNum = 0, P4_totalNum = 0, round;
-    public static bool isDiceUI = true;
+    public static bool isDiceUI = true, isDiceScene = false;
 
     public int min = 1;
     public int max = 7;
