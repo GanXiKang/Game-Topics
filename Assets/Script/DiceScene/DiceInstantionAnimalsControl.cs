e42c3aac40480e38223578770ad3f93181636e77
GanXiKang <gan0612xk@hotmail.com>
2023-07-05 20:46:56 +0800

    feat : 編寫生成程式碼

--- Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
+++ Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
@@ -7,6 +7,46 @@ public class DiceInstantionAnimalsControl : MonoBehaviour
     public GameObject[] animals = new GameObject[4];
     void Start()
     {
-        
+        if (Dice.who == 1)
+        {
+            for (int i = 1; i <= animals.Length; i++)
+            {
+                if (Menu_ChoosePlayer.whyP1 == i)
+                {
+                    Instantiate(animals[i - 1], transform.position, transform.rotation);
+                }
+            }
+        }
+        else if (Dice.who == 2)
+        {
+            for (int i = 1; i <= animals.Length; i++)
+            {
+                if (Menu_ChoosePlayer.whyP2 == i)
+                {
+                    Instantiate(animals[i - 1], transform.position, transform.rotation);
+                }
+            }
+        }
+        else if (Dice.who == 3)
+        {
+            for (int i = 1; i <= animals.Length; i++)
+            {
+                if (Menu_ChoosePlayer.whyP3 == i)
+                {
+                    Instantiate(animals[i - 1], transform.position, transform.rotation);
+                }
+            }
+        }
+        else if (Dice.who == 4)
+        {
+            for (int i = 1; i <= animals.Length; i++)
+            {
+                if (Menu_ChoosePlayer.whyP4 == i)
+                {
+                    Instantiate(animals[i - 1], transform.position, transform.rotation);
+                }
+            }
+        }
+
     }
 }
