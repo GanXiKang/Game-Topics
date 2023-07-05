db2cb2898f34f494890a440fdbce5c9af9c2bee0
GanXiKang <gan0612xk@hotmail.com>
2023-07-05 20:25:36 +0800

    feat ： 把以前的兩種方式 delete較複雜的

--- Assets/Script/DiceScene/DiceControl.cs
+++ Assets/Script/DiceScene/DiceControl.cs
@@ -8,15 +8,13 @@ public class DiceControl : MonoBehaviour
 
     public float rotationForce;
 
-    Quaternion stopQ;
-    Quaternion[] q = new Quaternion[7];
-
     int randomNum;
     bool isRoll, result;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        FindQuaternionEuler();
+
         isRoll = true;
     }
     void FixedUpdate()
@@ -53,45 +51,29 @@ public class DiceControl : MonoBehaviour
     void DiceRandomDice()
     {
         result = false;
-        stopQ = transform.rotation;
         rb.angularVelocity = Vector3.zero;
         randomNum = Random.Range(1, 7);
         print(randomNum);
         switch (randomNum)
         {
             case 1:
-                transform.rotation = Quaternion.Lerp(stopQ, q[1], 2f);
-                //transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+                transform.rotation = Quaternion.Euler(0f, 270f, 0f);
                 break;
             case 2:
-                transform.rotation = Quaternion.Lerp(stopQ, q[2], 2f);
-                //transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 break;
             case 3:
-                transform.rotation = Quaternion.Lerp(stopQ, q[3], 2f);
-                //transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                transform.rotation = Quaternion.Euler(90f, 0f, 0f);
                 break;
             case 4:
-                //transform.rotation = Quaternion.Lerp(stopQ, q[4], 2f);
                 transform.rotation = Quaternion.Euler(270f, 0f, 0f);
                 break;
             case 5:
-                //transform.rotation = Quaternion.Lerp(stopQ, q[5], 2f);
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 break;
             case 6:
-                //transform.rotation = Quaternion.Lerp(stopQ, q[6], 2f);
                 transform.rotation = Quaternion.Euler(0f, 90f, 0f);
                 break;
         }
-    }
-    void FindQuaternionEuler()
-    {
-        q[1] = Quaternion.Euler(0f, 270f, 0f);
-        q[2] = Quaternion.Euler(0f, 180f, 0f);
-        q[3] = Quaternion.Euler(90f, 0f, 0f);
-        q[4] = Quaternion.Euler(270f, 0f, 0f);
-        q[5] = Quaternion.Euler(0f, 0f, 0f);
-        q[6] = Quaternion.Euler(0f, 90f, 0f);
     }
 }
