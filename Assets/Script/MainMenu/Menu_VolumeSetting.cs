b2465aec1ff4eb8f2e4375c1660b8e75cfe1716f
GanXiKang <gan0612xk@hotmail.com>
2023-07-09 23:22:00 +0800

    feat ： 新建程式碼

---
+++ Assets/Script/MainMenu/Menu_VolumeSetting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private GameObject sliderBGM;
+    [SerializeField] private GameObject sliderEffects;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
