67d5d61d2b1ce61f9d374c679cd16cf4b8ea22f6
GanXiKang <gan0612xk@hotmail.com>
2023-07-09 20:10:51 +0800

    feat ： 優化程式碼

--- Assets/Script/MainMenu/Menu_ButtonControl.cs
+++ Assets/Script/MainMenu/Menu_ButtonControl.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 
 public class Menu_ButtonControl : MonoBehaviour
 {
-    public GameObject menu1;
-    public GameObject menu2;
-    public GameObject menu3;
-    public GameObject menu_t;
+    public GameObject[] menu = new GameObject[5];
 
     public AudioSource BGM;
     public AudioClip onClick, close;
 
     public void B_Start()
     {
-        menu1.SetActive(false);
-        menu2.SetActive(true);
+        menu[0].SetActive(false);
+        menu[1].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
     public void B_Tutorial()
     {
-        menu1.SetActive(false);
-        menu_t.SetActive(true);
+        menu[0].SetActive(false);
+        menu[3].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
     public void B_Setting()
     {
-        menu1.SetActive(false);
-        menu_t.SetActive(true);
+        menu[0].SetActive(false);
+        menu[4].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
     public void B_Exit()
@@ -37,30 +34,31 @@ public class Menu_ButtonControl : MonoBehaviour
     }
     public void B_Close()
     {
-        menu1.SetActive(true);
-        menu2.SetActive(false);
-        menu_t.SetActive(false);
+        menu[0].SetActive(true);
+        menu[1].SetActive(false);
+        menu[3].SetActive(false);
+        menu[4].SetActive(false);
         BGM.PlayOneShot(close);
     }
     public void B_2p()
     {
         Menu_ChoosePlayer.whoPlay = 2;
-        menu2.SetActive(false);
-        menu3.SetActive(true);
+        menu[1].SetActive(false);
+        menu[2].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
     public void B_3p()
     {
         Menu_ChoosePlayer.whoPlay = 3;
-        menu2.SetActive(false);
-        menu3.SetActive(true);
+        menu[1].SetActive(false);
+        menu[2].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
     public void B_4p()
     {
         Menu_ChoosePlayer.whoPlay = 4;
-        menu2.SetActive(false);
-        menu3.SetActive(true);
+        menu[1].SetActive(false);
+        menu[2].SetActive(true);
         BGM.PlayOneShot(onClick);
     }
 }
