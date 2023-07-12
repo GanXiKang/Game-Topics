e2cfb9579833321b0f30aab1565645326e7a460b
GanXiKang <gan0612xk@hotmail.com>
2023-07-12 21:39:41 +0800

    feat ： 編寫static去recordSetting

--- Assets/Script/MainMenu/Menu_VolumeSetting.cs
+++ Assets/Script/MainMenu/Menu_VolumeSetting.cs
@@ -8,8 +8,10 @@ public class Menu_VolumeSetting : MonoBehaviour
     public Slider sliderBGM;
     public Dropdown quality;
 
+    public static int index_record;
+    public static bool isFullS_record;
     public static float VolumeBGM = 0.7f;
-
+    
     AudioSource audio;
 
     void Start()
@@ -17,6 +19,8 @@ public class Menu_VolumeSetting : MonoBehaviour
         audio = GetComponent<AudioSource>();
 
         sliderBGM.value = VolumeBGM;
+        QualitySettings.SetQualityLevel(index_record);
+        Screen.fullScreen = isFullS_record;
     }
     public void Volume_BGM()
     {
@@ -26,9 +30,11 @@ public class Menu_VolumeSetting : MonoBehaviour
     public void Quality(int index)
     {
         QualitySettings.SetQualityLevel(index);
+        index_record = index;
     }
     public void FullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
+        isFullS_record = isFullScreen;
     }
 }
