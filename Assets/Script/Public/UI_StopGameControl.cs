ef8bd013558cbb51b2caa3802d0969368d12fe93
GanXiKang <gan0612xk@hotmail.com>
2023-07-13 22:16:08 +0800

    feat ： 編寫程式碼控制不同地方的setting

--- Assets/Script/Public/UI_StopGameControl.cs
+++ Assets/Script/Public/UI_StopGameControl.cs
@@ -6,16 +6,35 @@ using UnityEngine.SceneManagement;
 public class UI_StopGameControl : MonoBehaviour
 {
     public GameObject stopGameUI;
+
+    bool isSettingClose = false;
+
     void Start()
     {
         stopGameUI.SetActive(false);
     }
     void Update()
     {
+        BackToMainGame();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0f;
             stopGameUI.SetActive(true);
+        }
+    }
+    void BackToMainGame()
+    {
+        if (!MiniGameColliderControl.isMiniGame)
+        {
+            if (isSettingClose)
+            {
+                stopGameUI.SetActive(false);
+                isSettingClose = false;
+            }
+        }
+        else
+        {
+            isSettingClose = true;
         }
     }
     public void ContinueGame()
