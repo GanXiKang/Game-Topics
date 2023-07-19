17250f3d86830172d1e425bbb196ba399b1574de
GanXiKang <gan0612xk@hotmail.com>
2023-07-19 22:42:33 +0800

    feat ： 編寫findCamera

--- Assets/Script/MainGame/UI/RoundUIControl.cs
+++ Assets/Script/MainGame/UI/RoundUIControl.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoundUIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text whoRound;
+    public Text CoinTotal;
 
-    // Update is called once per frame
+    GameObject P1Camera;
+    GameObject P2Camera;
+    GameObject P3Camera;
+    GameObject P4Camera;
+
+    bool isFind = true;
+
     void Update()
     {
-        
+        if (!UI_LoadingControl.isLoading && isFind)
+        {
+            FindCamera();
+        }
+
+    }
+    void FindCamera()
+    {
+        P1Camera = GameObject.FindGameObjectWithTag("P1Camera");
+        P2Camera = GameObject.FindGameObjectWithTag("P2Camera");
+        P3Camera = GameObject.FindGameObjectWithTag("P3Camera");
+        P4Camera = GameObject.FindGameObjectWithTag("P4Camera");
+        isFind = false;
     }
 }
