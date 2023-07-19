8746700b47cbc87856c04f88df32d931ce8dbccd
GanXiKang <gan0612xk@hotmail.com>
2023-07-19 22:50:55 +0800

    feat ： 移除 findCamera的程式碼

--- Assets/Script/MainGame/UI/RoundUIControl.cs
+++ Assets/Script/MainGame/UI/RoundUIControl.cs
@@ -8,30 +8,8 @@ public class RoundUIControl : MonoBehaviour
     public Text whoRound;
     public Text CoinTotal;
 
-    GameObject P1Camera;
-    GameObject P2Camera;
-    GameObject P3Camera;
-    GameObject P4Camera;
-
-    bool isFind = true;
-
     void Update()
     {
-        if (!UI_LoadingControl.isLoading && isFind)
-        {
-            FindCamera();
-        }
-        if (P1Camera.GetComponent<Camera>().enabled)
-        {
-            print("Yes");
-        }
-    }
-    void FindCamera()
-    {
-        P1Camera = GameObject.FindGameObjectWithTag("P1Camera");
-        P2Camera = GameObject.FindGameObjectWithTag("P2Camera");
-        P3Camera = GameObject.FindGameObjectWithTag("P3Camera");
-        P4Camera = GameObject.FindGameObjectWithTag("P4Camera");
-        isFind = false;
+
     }
 }
