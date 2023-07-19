a00cff5254c006b33f6c355647b47e8899951e56
GanXiKang <gan0612xk@hotmail.com>
2023-07-19 22:56:21 +0800

    feat ： 編寫程式碼

--- Assets/Script/MainGame/UI/RoundUIControl.cs
+++ Assets/Script/MainGame/UI/RoundUIControl.cs
@@ -10,6 +10,31 @@ public class RoundUIControl : MonoBehaviour
 
     void Update()
     {
+        switch (ChangeCameraControl.changeCameraNum)
+        {
+            case 1:
+                whoRound.text = "P1";
+                whoRound.color = Color.red;
+                CoinTotal.text = "Coin :" + CoinControl.P1CoinTotal.ToString();
+                break;
 
+            case 2:
+                whoRound.text = "P2";
+                whoRound.color = Color.yellow;
+                CoinTotal.text = "Coin :" + CoinControl.P2CoinTotal.ToString();
+                break;
+
+            case 3:
+                whoRound.text = "P3";
+                whoRound.color = Color.green;
+                CoinTotal.text = "Coin :" + CoinControl.P3CoinTotal.ToString();
+                break;
+
+            case 4:
+                whoRound.text = "P4";
+                whoRound.color = Color.blue;
+                CoinTotal.text = "Coin :" + CoinControl.P4CoinTotal.ToString();
+                break;
+        }
     }
 }
