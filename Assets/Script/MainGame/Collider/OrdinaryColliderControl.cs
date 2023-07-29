ac4421585400bb01981a9c14a1673dbc3d2d5ab6
GanXiKang <gan0612xk@hotmail.com>
2023-07-29 22:39:27 +0800

    feat : 編寫程式碼

--- Assets/Script/MainGame/Collider/OrdinaryColliderControl.cs
+++ Assets/Script/MainGame/Collider/OrdinaryColliderControl.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class OrdinaryColliderControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int pointNum;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (DiceControl.P1_totalNum == pointNum || DiceControl.P2_totalNum == pointNum || DiceControl.P3_totalNum == pointNum || DiceControl.P4_totalNum == pointNum)
+        {
+            if (other.tag == "P1")
+            {
+                AnimatorControl.isP1Move = false;
+                StartCoroutine(ChangeCamera());
+            }
+            else if (other.tag == "P2")
+            {
+                AnimatorControl.isP2Move = false;
+                StartCoroutine(ChangeCamera());
+            }
+            else if (other.tag == "P3")
+            {
+                AnimatorControl.isP3Move = false;
+                StartCoroutine(ChangeCamera());
+            }
+            else if (other.tag == "P4")
+            {
+                AnimatorControl.isP4Move = false;
+                StartCoroutine(ChangeCamera());
+            }
+        }
+    }
+    IEnumerator ChangeCamera()
+    {
+        yield return new WaitForSeconds(1f);
+        ChangeCameraControl.changeCameraNum++;
+        Dice.isDiceUI = true;
     }
 }
