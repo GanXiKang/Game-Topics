270981d70aac6c4f53396f593601a13e8592680b
GanXiKang <gan0612xk@hotmail.com>
2023-08-04 22:47:49 +0800

    feat ： 嘗試將兩者程式碼合在一起

--- Assets/Script/MainGame/Ocean/InsKickboardControl.cs
+++ Assets/Script/MainGame/Ocean/InsKickboardControl.cs
@@ -9,6 +9,17 @@ public class InsKickboardControl : MonoBehaviour
 
     public static bool isFollow;
 
+    void Update()
+    {
+        if (isFollow)
+        {
+            kickboard.transform.position = insPoint.transform.position;
+        }
+        else
+        {
+            Destroy(kickboard, 2f);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Ocean")
