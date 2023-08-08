a15f893a05568ab5799cae37cb4e43212bdf72d2
GanXiKang <gan0612xk@hotmail.com>
2023-08-08 23:32:52 +0800

    feat ： 同下

--- Assets/Script/MainGame/Collider/StoreColliderControl.cs
+++ Assets/Script/MainGame/Collider/StoreColliderControl.cs
@@ -70,7 +70,7 @@ public class StoreColliderControl : MonoBehaviour
 
     IEnumerator LookTarget()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         PlayerMoveControl.isMove = false;
     }
 }
