3700625c8c33d04269b28ee1c43d2b929fa9279b
GanXiKang <gan0612xk@hotmail.com>
2023-08-16 23:27:10 +0800

    feat ： 編寫第1組UI的程式碼

--- Assets/Script/MainGame/Store/StoreUIControl.cs
+++ Assets/Script/MainGame/Store/StoreUIControl.cs
@@ -7,15 +7,32 @@ public class StoreUIControl : MonoBehaviour
 {
     public GameObject[] StoreUI = new GameObject[4];
 
-    public static bool isStore;
-
-    void Start()
-    {
-        
-    }
+    public static bool isStore, isBuying;
 
     void Update()
     {
-        
+        if (isStore)
+        {
+            StoreUI[1].SetActive(true);
+            isStore = false;
+        }
+    }
+
+    public void Button_Yes()
+    {
+        isBuying = true;
+        StoreUI[1].SetActive(false);
+        StoreUI[2].SetActive(true);
+    }
+    public void Button_NO()
+    {
+        StartCoroutine(ChangeCamera());
+    }
+    IEnumerator ChangeCamera()
+    {
+        StoreUI[1].SetActive(false);
+        yield return new WaitForSeconds(1f);
+        ChangeCameraControl.changeCameraNum++;
+        Dice.isDiceUI = true;
     }
 }
