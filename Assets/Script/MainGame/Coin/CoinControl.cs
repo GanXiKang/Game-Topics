b22d01e090feb1baebf6041259ecca8772e56cdf
GanXiKang <gan0612xk@hotmail.com>
2023-08-17 01:06:08 +0800

    feat ： 公開比較方便調整

--- Assets/Script/MainGame/Coin/CoinControl.cs
+++ Assets/Script/MainGame/Coin/CoinControl.cs
@@ -6,13 +6,13 @@ public class CoinControl : MonoBehaviour
 {
     public static int P1CoinTotal, P2CoinTotal, P3CoinTotal, P4CoinTotal;
 
-    int Coin = 80;
+    public int initialCoin = 80;
 
     private void Start()
     {
-        P1CoinTotal = Coin;
-        P2CoinTotal = Coin;
-        P3CoinTotal = Coin;
-        P4CoinTotal = Coin;
+        P1CoinTotal = initialCoin;
+        P2CoinTotal = initialCoin;
+        P3CoinTotal = initialCoin;
+        P4CoinTotal = initialCoin;
     }
 }
