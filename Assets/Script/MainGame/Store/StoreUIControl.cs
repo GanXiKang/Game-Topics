c5008cf106a59d93badef84a6ec912b6edb4f681
GanXiKang <gan0612xk@hotmail.com>
2023-08-17 01:16:53 +0800

    feat ： 編寫針對who去判斷CoinTotal

--- Assets/Script/MainGame/Store/StoreUIControl.cs
+++ Assets/Script/MainGame/Store/StoreUIControl.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class StoreUIControl : MonoBehaviour
 {
     public GameObject[] StoreUI = new GameObject[4];
-    public Text[] howMuchCoin = new Text[7];
+    public Text[] PorpsCoin = new Text[7];
     public Text[] group3Text = new Text[5];
 
     public static bool isStore = false, isBuying = false;
 
+    int[] howMuchCoin = new int[7];
     int whyProps, buyQuantity = 1;
     void Update()
     {
@@ -21,7 +22,244 @@ public class StoreUIControl : MonoBehaviour
         }
         else if (isBuying)
         {
-            
+            switch (StoreColliderControl.who)
+            {
+                case 1:
+                    if (CoinControl.P1CoinTotal < howMuchCoin[0])
+                    {
+                        PorpsCoin[0].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[0].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[1])
+                    {
+                        PorpsCoin[1].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[1].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[2])
+                    {
+                        PorpsCoin[2].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[2].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[3])
+                    {
+                        PorpsCoin[3].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[3].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[4])
+                    {
+                        PorpsCoin[4].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[4].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[5])
+                    {
+                        PorpsCoin[5].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[5].color = Color.green;
+                    }
+                    if (CoinControl.P1CoinTotal < howMuchCoin[6])
+                    {
+                        PorpsCoin[6].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[6].color = Color.green;
+                    }
+                    break;
+
+                case 2:
+                    if (CoinControl.P2CoinTotal < howMuchCoin[0])
+                    {
+                        PorpsCoin[0].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[0].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[1])
+                    {
+                        PorpsCoin[1].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[1].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[2])
+                    {
+                        PorpsCoin[2].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[2].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[3])
+                    {
+                        PorpsCoin[3].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[3].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[4])
+                    {
+                        PorpsCoin[4].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[4].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[5])
+                    {
+                        PorpsCoin[5].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[5].color = Color.green;
+                    }
+                    if (CoinControl.P2CoinTotal < howMuchCoin[6])
+                    {
+                        PorpsCoin[6].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[6].color = Color.green;
+                    }
+                    break;
+
+                case 3:
+                    if (CoinControl.P3CoinTotal < howMuchCoin[0])
+                    {
+                        PorpsCoin[0].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[0].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[1])
+                    {
+                        PorpsCoin[1].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[1].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[2])
+                    {
+                        PorpsCoin[2].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[2].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[3])
+                    {
+                        PorpsCoin[3].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[3].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[4])
+                    {
+                        PorpsCoin[4].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[4].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[5])
+                    {
+                        PorpsCoin[5].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[5].color = Color.green;
+                    }
+                    if (CoinControl.P3CoinTotal < howMuchCoin[6])
+                    {
+                        PorpsCoin[6].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[6].color = Color.green;
+                    }
+                    break;
+
+                case 4:
+                    if (CoinControl.P4CoinTotal < howMuchCoin[0])
+                    {
+                        PorpsCoin[0].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[0].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[1])
+                    {
+                        PorpsCoin[1].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[1].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[2])
+                    {
+                        PorpsCoin[2].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[2].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[3])
+                    {
+                        PorpsCoin[3].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[3].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[4])
+                    {
+                        PorpsCoin[4].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[4].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[5])
+                    {
+                        PorpsCoin[5].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[5].color = Color.green;
+                    }
+                    if (CoinControl.P4CoinTotal < howMuchCoin[6])
+                    {
+                        PorpsCoin[6].color = Color.red;
+                    }
+                    else
+                    {
+                        PorpsCoin[6].color = Color.green;
+                    }
+                    break;
+            }
         }
     }
 
