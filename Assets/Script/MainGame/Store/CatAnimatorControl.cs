6558b693eade19fac229aebf836b5be219d56d53
GanXiKang <gan0612xk@hotmail.com>
2023-08-17 21:35:29 +0800

    feat ： 編寫Cat動作程式碼

--- Assets/Script/MainGame/Store/CatAnimatorControl.cs
+++ Assets/Script/MainGame/Store/CatAnimatorControl.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CatAnimatorControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator ani;
+
+    public static bool isHappy = false;
+    public static bool isSad = false;
+    public static bool isWave = false;
+    public static bool isBye = false;
+
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isHappy)
+        {
+            ani.SetBool("Happy", true);
+        }
+        else 
+        {
+            ani.SetBool("Happy", false);
+        }
+
+        if (isSad)
+        {
+            ani.SetBool("Sad", true);
+        }
+        else
+        {
+            ani.SetBool("Sad", false);
+        }
+
+        if (isWave)
+        {
+            ani.SetBool("Wave", true);
+        }
+        else
+        {
+            ani.SetBool("Wave", false);
+        }
+
+        if (isBye)
+        {
+            ani.SetBool("Bye", true);
+        }
+        else
+        {
+            ani.SetBool("Bye", false);
+        }
     }
 }
