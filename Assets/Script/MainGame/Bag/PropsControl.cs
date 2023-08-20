2ce33d4ec36a1a31143c0decc43428dab64e3889
GanXiKang <gan0612xk@hotmail.com>
2023-08-20 19:12:27 +0800

    fix ： 改名

--- Assets/Script/MainGame/Bag/PropsControl.cs
+++ Assets/Script/MainGame/Bag/PropsControl.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PropsControl : MonoBehaviour
 {
-    public static int[] P1Porps = new int[7];
-    public static int[] P2Porps = new int[7];
-    public static int[] P3Porps = new int[7];
-    public static int[] P4Porps = new int[7];
+    public static int[] P1Props = new int[7];
+    public static int[] P2Props = new int[7];
+    public static int[] P3Props = new int[7];
+    public static int[] P4Props = new int[7];
 
     void Start()
     {
@@ -16,37 +16,37 @@ public class PropsControl : MonoBehaviour
 
     void Update()
     {
-
+      
     }
     void Initial()
     {
-        P1Porps[0] = 0;
-        P1Porps[1] = 0;
-        P1Porps[2] = 0;
-        P1Porps[3] = 0;
-        P1Porps[4] = 0;
-        P1Porps[5] = 0;
-        P1Porps[6] = 0;
-        P2Porps[0] = 0;
-        P2Porps[1] = 0;
-        P2Porps[2] = 0;
-        P2Porps[3] = 0;
-        P2Porps[4] = 0;
-        P2Porps[5] = 0;
-        P2Porps[6] = 0;
-        P3Porps[0] = 0;
-        P3Porps[1] = 0;
-        P3Porps[2] = 0;
-        P3Porps[3] = 0;
-        P3Porps[4] = 0;
-        P3Porps[5] = 0;
-        P3Porps[6] = 0;
-        P4Porps[0] = 0;
-        P4Porps[1] = 0;
-        P4Porps[2] = 0;
-        P4Porps[3] = 0;
-        P4Porps[4] = 0;
-        P4Porps[5] = 0;
-        P4Porps[6] = 0;
+        P1Props[0] = 0;
+        P1Props[1] = 0;
+        P1Props[2] = 0;
+        P1Props[3] = 0;
+        P1Props[4] = 0;
+        P1Props[5] = 0;
+        P1Props[6] = 0;
+        P2Props[0] = 0;
+        P2Props[1] = 0;
+        P2Props[2] = 0;
+        P2Props[3] = 0;
+        P2Props[4] = 0;
+        P2Props[5] = 0;
+        P2Props[6] = 0;
+        P3Props[0] = 0;
+        P3Props[1] = 0;
+        P3Props[2] = 0;
+        P3Props[3] = 0;
+        P3Props[4] = 0;
+        P3Props[5] = 0;
+        P3Props[6] = 0;
+        P4Props[0] = 0;
+        P4Props[1] = 0;
+        P4Props[2] = 0;
+        P4Props[3] = 0;
+        P4Props[4] = 0;
+        P4Props[5] = 0;
+        P4Props[6] = 0;
     }
 }
