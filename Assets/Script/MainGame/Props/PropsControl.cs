2735033bedc6dfeed12b9c5d5b41b7706d2b8305
GanXiKang <gan0612xk@hotmail.com>
2023-08-20 19:44:31 +0800

    feat ： 編寫觀察道具數量

--- Assets/Script/MainGame/Props/PropsControl.cs
+++ Assets/Script/MainGame/Props/PropsControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PropsControl : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PropsControl : MonoBehaviour
     public static int[] P3Props = new int[7];
     public static int[] P4Props = new int[7];
 
+    public Text[] PropsQuantity = new Text[7];
+
     void Start()
     {
         Initial();
@@ -16,7 +19,48 @@ public class PropsControl : MonoBehaviour
 
     void Update()
     {
+        switch (ChangeCameraControl.changeCameraNum)
+        {
+            case 1:
+                PropsQuantity[0].text = P1Props[0].ToString();
+                PropsQuantity[1].text = P1Props[1].ToString();
+                PropsQuantity[2].text = P1Props[2].ToString();
+                PropsQuantity[3].text = P1Props[3].ToString();
+                PropsQuantity[4].text = P1Props[4].ToString();
+                PropsQuantity[5].text = P1Props[5].ToString();
+                PropsQuantity[6].text = P1Props[6].ToString();
+                break;
 
+            case 2:
+                PropsQuantity[0].text = P2Props[0].ToString();
+                PropsQuantity[1].text = P2Props[1].ToString();
+                PropsQuantity[2].text = P2Props[2].ToString();
+                PropsQuantity[3].text = P2Props[3].ToString();
+                PropsQuantity[4].text = P2Props[4].ToString();
+                PropsQuantity[5].text = P2Props[5].ToString();
+                PropsQuantity[6].text = P2Props[6].ToString();
+                break;
+
+            case 3:
+                PropsQuantity[0].text = P3Props[0].ToString();
+                PropsQuantity[1].text = P3Props[1].ToString();
+                PropsQuantity[2].text = P3Props[2].ToString();
+                PropsQuantity[3].text = P3Props[3].ToString();
+                PropsQuantity[4].text = P3Props[4].ToString();
+                PropsQuantity[5].text = P3Props[5].ToString();
+                PropsQuantity[6].text = P3Props[6].ToString();
+                break;
+
+            case 4:
+                PropsQuantity[0].text = P4Props[0].ToString();
+                PropsQuantity[1].text = P4Props[1].ToString();
+                PropsQuantity[2].text = P4Props[2].ToString();
+                PropsQuantity[3].text = P4Props[3].ToString();
+                PropsQuantity[4].text = P4Props[4].ToString();
+                PropsQuantity[5].text = P4Props[5].ToString();
+                PropsQuantity[6].text = P4Props[6].ToString();
+                break;
+        }
     }
     void Initial()
     {
