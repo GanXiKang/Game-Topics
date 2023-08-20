fcb4f2786849726e7912c8cad54619fe675f4b6b
GanXiKang <gan0612xk@hotmail.com>
2023-08-20 18:39:41 +0800

    feat ： set 初始值

--- Assets/Script/MainGame/Bag/PorpsControl.cs
+++ Assets/Script/MainGame/Bag/PorpsControl.cs
@@ -11,11 +11,42 @@ public class PorpsControl : MonoBehaviour
 
     void Start()
     {
-        
+        Initial();
     }
 
     void Update()
     {
         
+    }
+    void Initial()
+    {
+        P1Porps[0] = 0;
+        P1Porps[1] = 0;
+        P1Porps[2] = 0;
+        P1Porps[3] = 0;
+        P1Porps[4] = 0;
+        P1Porps[5] = 0;
+        P1Porps[6] = 0;
+        P2Porps[0] = 0;
+        P2Porps[1] = 0;
+        P2Porps[2] = 0;
+        P2Porps[3] = 0;
+        P2Porps[4] = 0;
+        P2Porps[5] = 0;
+        P2Porps[6] = 0;
+        P3Porps[0] = 0;
+        P3Porps[1] = 0;
+        P3Porps[2] = 0;
+        P3Porps[3] = 0;
+        P3Porps[4] = 0;
+        P3Porps[5] = 0;
+        P3Porps[6] = 0;
+        P4Porps[0] = 0;
+        P4Porps[1] = 0;
+        P4Porps[2] = 0;
+        P4Porps[3] = 0;
+        P4Porps[4] = 0;
+        P4Porps[5] = 0;
+        P4Porps[6] = 0;
     }
 }
