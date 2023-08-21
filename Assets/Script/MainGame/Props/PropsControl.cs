ff841a8dce84260bb8362aec00ce8fc1c1ff8107
GanXiKang <gan0612xk@hotmail.com>
2023-08-22 00:34:19 +0800

    feat ： 調整初始值測試

--- Assets/Script/MainGame/Props/PropsControl.cs
+++ Assets/Script/MainGame/Props/PropsControl.cs
@@ -64,10 +64,10 @@ public class PropsControl : MonoBehaviour
     }
     void Initial()
     {
-        P1Props[0] = 0;
-        P1Props[1] = 0;
-        P1Props[2] = 0;
-        P1Props[3] = 0;
+        P1Props[0] = 1;
+        P1Props[1] = 2;
+        P1Props[2] = 5;
+        P1Props[3] = 4;
         P1Props[4] = 0;
         P1Props[5] = 0;
         P1Props[6] = 0;
