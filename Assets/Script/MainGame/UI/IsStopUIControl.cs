1b003b6a0ea4841b633a7bfd910f811d02fed87a
GanXiKang <gan0612xk@hotmail.com>
2023-08-24 01:24:05 +0800

    feat ： 編寫相關程式碼

---
+++ Assets/Script/MainGame/UI/IsStopUIControl.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IsStopUIControl : MonoBehaviour
+{
+    public GameObject interactiveUI;
+    public Text systemText;
+
+    public static bool isStopUI;
+
+    void Update()
+    {
+        if (isStopUI)
+        {
+            switch (ChangeCameraControl.changeCameraNum)
+            {
+                case 1:
+                    if (EventControl.isStopP1)
+                    {
+                        StartCoroutine(StopThisRound());
+                    }
+                    break;
+
+                case 2:
+                    if (EventControl.isStopP2)
+                    {
+                        StartCoroutine(StopThisRound());
+                    }
+                    break;
+
+                case 3:
+                    if (EventControl.isStopP3)
+                    {
+                        StartCoroutine(StopThisRound());
+                    }
+                    break;
+
+                case 4:
+                    if (EventControl.isStopP4)
+                    {
+                        StartCoroutine(StopThisRound());
+                    }
+                    break;
+            }
+        }
+    }
+    IEnumerator StopThisRound()
+    {
+        isStopUI = false;
+        DiceUIControl.isDiceUI = false;
+        systemText.text = "�˻غϕ�ͣ";
+        yield return new WaitForSeconds(2f);
+        ChangeCameraControl.changeCameraNum++;
+        DiceUIControl.isDiceUI = true;
+    }
+}
