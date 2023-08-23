4e23853eeb0885003eb86f75866c9d77e15a6bb3
GanXiKang <gan0612xk@hotmail.com>
2023-08-24 02:04:37 +0800

    feat ： delete 舊的程式碼

--- Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
+++ Assets/Script/DiceScene/DiceInstantionAnimalsControl.cs
@@ -50,45 +50,5 @@ public class DiceInstantionAnimalsControl : MonoBehaviour
                 }
                 break;
         }
-        //if (DiceUIControl.who == 1)
-        //{
-        //    for (int i = 1; i <= animals.Length; i++)
-        //    {
-        //        if (Menu_ChoosePlayer.whyP1 == i)
-        //        {
-        //            Instantiate(animals[i - 1], transform.position, transform.rotation);
-        //        }
-        //    }
-        //}
-        //else if (DiceUIControl.who == 2)
-        //{
-        //    for (int i = 1; i <= animals.Length; i++)
-        //    {
-        //        if (Menu_ChoosePlayer.whyP2 == i)
-        //        {
-        //            Instantiate(animals[i - 1], transform.position, transform.rotation);
-        //        }
-        //    }
-        //}
-        //else if (DiceUIControl.who == 3)
-        //{
-        //    for (int i = 1; i <= animals.Length; i++)
-        //    {
-        //        if (Menu_ChoosePlayer.whyP3 == i)
-        //        {
-        //            Instantiate(animals[i - 1], transform.position, transform.rotation);
-        //        }
-        //    }
-        //}
-        //else if (DiceUIControl.who == 4)
-        //{
-        //    for (int i = 1; i <= animals.Length; i++)
-        //    {
-        //        if (Menu_ChoosePlayer.whyP4 == i)
-        //        {
-        //            Instantiate(animals[i - 1], transform.position, transform.rotation);
-        //        }
-        //    }
-        //}
     }
 }
