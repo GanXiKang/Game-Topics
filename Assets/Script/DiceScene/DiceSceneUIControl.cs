5590f92ebd4d0f559abe1bf02931223b6f867b5b
GanXiKang <gan0612xk@hotmail.com>
2023-08-31 01:01:37 +0800

    feat 編寫code

--- Assets/Script/DiceScene/DiceSceneUIControl.cs
+++ Assets/Script/DiceScene/DiceSceneUIControl.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceSceneUIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject normalUI, renewUI, doubleUI, customUI;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (BagUIControl.isDoubleDice)
+        {
+            doubleUI.SetActive(true);
+        }
+        if (BagUIControl.isCustomDice)
+        {
+            customUI.SetActive(true);
+            normalUI.SetActive(false);
+        }
     }
 }
