c1f5040290d4763986a60949adb59d8d89cbc6cb
GanXiKang <gan0612xk@hotmail.com>
2023-09-04 23:09:47 +0800

    feat : 編寫code

--- Assets/Script/MainGame/Props/BombPropsControl.cs
+++ Assets/Script/MainGame/Props/BombPropsControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BombPropsControl : MonoBehaviour
 {
     public static int pointNum;
+    public static bool isBoomStopP1, isBoomStopP2, isBoomStopP3, isBoomStopP4;
 
     void FixedUpdate()
     {
@@ -13,6 +14,7 @@ public class BombPropsControl : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        IsStopUIControl.isStopUI++;
+        isBoomStopP1 = true;
     }
 }
