684c13f0d1c5649e5bd3eba9b34584fffb5b04c2
GanXiKang <gan0612xk@hotmail.com>
2023-09-05 00:00:27 +0800

    feat ： delete 死碼

--- Assets/Script/MainGame/Animals/InsBombControl.cs
+++ Assets/Script/MainGame/Animals/InsBombControl.cs
@@ -6,7 +6,6 @@ public class InsBombControl : MonoBehaviour
 {
     public GameObject bombProps;
     public Transform insBombPoint;
-    public static bool isP1InsBomb, isP2InsBomb, isP3InsBomb, isP4InsBomb;
 
     void Update()
     {
@@ -17,7 +16,6 @@ public class InsBombControl : MonoBehaviour
                 case 1:
                     if (gameObject.tag == "P1")
                     {
-                        isP1InsBomb = true;
                         BombPropsControl.pointNum = DiceControl.P1_totalNum;
                         Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
                     }
@@ -26,7 +24,6 @@ public class InsBombControl : MonoBehaviour
                 case 2:
                     if (gameObject.tag == "P2")
                     {
-                        isP2InsBomb = true;
                         BombPropsControl.pointNum = DiceControl.P2_totalNum;
                         Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
                     }
@@ -35,7 +32,6 @@ public class InsBombControl : MonoBehaviour
                 case 3:
                     if (gameObject.tag == "P3")
                     {
-                        isP3InsBomb = true;
                         BombPropsControl.pointNum = DiceControl.P3_totalNum;
                         Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
                     }
@@ -44,7 +40,6 @@ public class InsBombControl : MonoBehaviour
                 case 4:
                     if (gameObject.tag == "P4")
                     {
-                        isP4InsBomb = true;
                         BombPropsControl.pointNum = DiceControl.P4_totalNum; 
                         Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
                     }
