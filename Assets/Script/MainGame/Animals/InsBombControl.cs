770f7d9c961fff2de6f16928cfaf8309a7de41d1
GanXiKang <gan0612xk@hotmail.com>
2023-09-04 19:51:54 +0800

    feat ： 編寫生成

--- Assets/Script/MainGame/Animals/InsBombControl.cs
+++ Assets/Script/MainGame/Animals/InsBombControl.cs
@@ -11,7 +11,41 @@ public class InsBombControl : MonoBehaviour
     {
         if (BagUIControl.isBomb)
         {
+            switch (ChangeCameraControl.changeCameraNum)
+            {
+                case 1:
+                    if (gameObject.tag == "P1")
+                    {
+                        DiceControl.P1_totalNum = BombPropsControl.pointNum;
+                        Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
+                    }
+                    break;
 
+                case 2:
+                    if (gameObject.tag == "P2")
+                    {
+                        DiceControl.P2_totalNum = BombPropsControl.pointNum;
+                        Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
+                    }
+                    break;
+
+                case 3:
+                    if (gameObject.tag == "P3")
+                    {
+                        DiceControl.P3_totalNum = BombPropsControl.pointNum;
+                        Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
+                    }
+                    break;
+
+                case 4:
+                    if (gameObject.tag == "P4")
+                    {
+                        DiceControl.P4_totalNum = BombPropsControl.pointNum;
+                        Instantiate(bombProps, insBombPoint.position, insBombPoint.rotation);
+                    }
+                    break;
+            }
+            BagUIControl.isBomb = false;
         }
     }
 }
