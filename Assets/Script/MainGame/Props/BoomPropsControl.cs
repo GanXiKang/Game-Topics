8c86cf3187445e62f0faf20c1fb114800d3d978f
GanXiKang <gan0612xk@hotmail.com>
2023-09-04 17:30:30 +0800

    feat ： 編寫自轉

--- Assets/Script/MainGame/Props/BoomPropsControl.cs
+++ Assets/Script/MainGame/Props/BoomPropsControl.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class BoomPropsControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0, -1, 0);
+    }
     void Update()
     {
         
