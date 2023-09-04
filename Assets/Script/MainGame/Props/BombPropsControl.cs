98cf7548a4e038faa1baf3a5fc98671cb830a012
GanXiKang <gan0612xk@hotmail.com>
2023-09-04 23:29:43 +0800

    feat ： 把自轉拿掉

--- Assets/Script/MainGame/Props/BombPropsControl.cs
+++ Assets/Script/MainGame/Props/BombPropsControl.cs
@@ -7,11 +7,6 @@ public class BombPropsControl : MonoBehaviour
     public static int pointNum;
     public static bool isBoomStopP1, isBoomStopP2, isBoomStopP3, isBoomStopP4;
 
-    void FixedUpdate()
-    {
-        transform.Rotate(0, -1, 0);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         IsStopUIControl.isStopUI++;
