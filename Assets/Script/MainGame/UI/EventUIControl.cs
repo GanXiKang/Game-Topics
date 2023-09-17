4ec904e5a94f235bebdde09d3878f42d861a6b50
GanXiKang <gan0612xk@hotmail.com>
2023-09-17 13:37:42 +0800

    feat ： 新建程式

---
+++ Assets/Script/MainGame/UI/EventUIControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventUIControl : MonoBehaviour
+{
+    public GameObject eventImage[];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
