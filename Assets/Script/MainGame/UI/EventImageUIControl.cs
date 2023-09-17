7ddf712c3d6b1b662fe5b5046ccfefeed2a40bd0
GanXiKang <gan0612xk@hotmail.com>
2023-09-17 14:35:23 +0800

    feat ： 新的程式

---
+++ Assets/Script/MainGame/UI/EventImageUIControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventImageUIControl : MonoBehaviour
+{
+    public GameObject[] eventImage = new GameObject[5];
+
+    public static int eventPointNum, eventAB;
+
+    void Update()
+    {
+        if (eventPointNum == 2)
+        {
+            
+        }
+    }
+}
