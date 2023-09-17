898dc8dc6133d8bf01dffc3102dd569b7a488e28
GanXiKang <gan0612xk@hotmail.com>
2023-09-18 00:44:33 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/UI/SystemTestTextControl.cs
+++ Assets/Script/MainGame/UI/SystemTestTextControl.cs
@@ -7,17 +7,21 @@ public class SystemTestTextControl : MonoBehaviour
 {
     float timer = 0;
     public Text systemText;
+    public GameObject backGround;
+
     public static bool isTimer = false;
     void FixedUpdate()
     {
         if (isTimer)
         {
+            backGround.SetActive(true);
             timer += 1 * Time.deltaTime;
             if (timer > 2f)
             {
                 systemText.text = " ";
                 timer = 0;
                 isTimer = false;
+                backGround.SetActive(false);
             }
         }
     }
