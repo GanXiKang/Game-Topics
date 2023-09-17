9f35bc13a8d7ada2af4153759721cf1b6d9fb6d6
GanXiKang <gan0612xk@hotmail.com>
2023-09-18 02:06:16 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/UI/EventImageUIControl.cs
+++ Assets/Script/MainGame/UI/EventImageUIControl.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EventImageUIControl : MonoBehaviour
 {
+    float Timer = 0;
+
     public GameObject[] eventImage = new GameObject[5];
 
     public static int eventPointNum, eventAB;
+    public static bool isTimer;
 
     void Update()
     {
-        if (eventPointNum == 2)
+        if (isTimer)
         {
-            
+            Timer += 1 * Time.deltaTime;
+
+            if (eventPointNum == 2)
+            {
+                eventImage[0].SetActive(true);
+                Timing();
+            }
+        }
+    }
+    void Timing()
+    {
+        if (Timer > 2)
+        {
+            isTimer = false;
+            eventPointNum = 0;
+            eventAB = 0;
+            for (int i = 0; i < eventImage.Length; i++)
+            {
+                eventImage[i].SetActive(false);
+            }
         }
     }
 }
