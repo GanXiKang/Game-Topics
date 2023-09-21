04221fb504af52f568e12b2beb24e117b0b15379
GanXiKang <gan0612xk@hotmail.com>
2023-09-22 00:59:47 +0800

    feat ： 錯字

--- Assets/Script/MainGame/Power/AnimalsPowerControl.cs
+++ Assets/Script/MainGame/Power/AnimalsPowerControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimalsPowerControl : MonoBehaviour
 {
-    public static bool isMousePower = false, isCowPower = false, isTIgerPower = false, isRabbitPower = false;
+    public static bool isMousePower = false, isCowPower = false, isTigerPower = false, isRabbitPower = false;
     public static bool isDragonPower = false, isSnakePower = false, isHousePower = false, isSheepPower = false;
     public static bool isMonkeyPower = false, isChickenPower = false, isDogPower = false, isPigPower = false;
 
