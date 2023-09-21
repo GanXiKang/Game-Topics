1971223361b402771e917d7c938a7e1a949de1b7
GanXiKang <gan0612xk@hotmail.com>
2023-09-22 00:41:42 +0800

    feat : 優化

--- Assets/Script/MainGame/Power/AnimalsPowerControl.cs
+++ Assets/Script/MainGame/Power/AnimalsPowerControl.cs
@@ -7,6 +7,7 @@ public class AnimalsPowerControl : MonoBehaviour
     public static bool isMousePower = false, isCowPower = false, isTIgerPower = false, isRabbitPower = false;
     public static bool isDragonPower = false, isSnakePower = false, isHousePower = false, isSheepPower = false;
     public static bool isMonkeyPower = false, isChickenPower = false, isDogPower = false, isPigPower = false;
+
     void Start()
     {
         
