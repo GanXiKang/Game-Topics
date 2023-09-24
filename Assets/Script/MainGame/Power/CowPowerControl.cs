f2707b44ef3181ea5ed75763d08b313f74235a3b
GanXiKang <gan0612xk@hotmail.com>
2023-09-25 02:15:28 +0800

    feat : 編寫Cow speed

--- Assets/Script/MainGame/Power/CowPowerControl.cs
+++ Assets/Script/MainGame/Power/CowPowerControl.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CowPowerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    NavMeshAgent agent;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (AnimalsPowerControl.cowUsePower)
+        {
+            agent.speed = 20;
+        }
+        else 
+        {
+            agent.speed = 10;
+        }
     }
 }
