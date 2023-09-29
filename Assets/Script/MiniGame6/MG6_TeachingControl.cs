51625242f3475bb9589babfac43cd0febfcc6895
GanXiKang <gan0612xk@hotmail.com>
2023-09-30 00:12:53 +0800

    feat : 更改

--- Assets/Script/MiniGame6/MG6_TeachingControl.cs
+++ Assets/Script/MiniGame6/MG6_TeachingControl.cs
@@ -6,7 +6,7 @@ public class MG6_TeachingControl : MonoBehaviour
 {
     public GameObject teach, mouse1, mouse2;
 
-    void Start()
+    void Update()
     {
         StartCoroutine(CloseTeach());
     }
