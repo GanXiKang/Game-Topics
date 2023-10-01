18668bd6e443166c4d4c9c37a2c2743f1b220881
GanXiKang <gan0612xk@hotmail.com>
2023-10-02 01:00:22 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/MGAward/MGFinishAwardControl.cs
+++ Assets/Script/MainGame/MGAward/MGFinishAwardControl.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MGFinishAwardControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int miniGame;
+    public static bool isFinishMG = false, iswin;
 
-    // Update is called once per frame
+    public Text systemText;
+
+    int winCoin = 30, loseCoin = 10;
+    bool isCoinAward;
+
     void Update()
     {
-        
+        if (isFinishMG)
+        {
+            switch (miniGame)
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+            }
+            isFinishMG = false;
+        }
     }
 }
