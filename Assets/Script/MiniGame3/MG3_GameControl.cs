5ecd4b3822eaa3911dca20e33563b5749029baea
GanXiKang <gan0612xk@hotmail.com>
2023-10-06 23:34:17 +0800

    feat : new Script

---
+++ Assets/Script/MiniGame3/MG3_GameControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG3_GameControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
