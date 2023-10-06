616e34e4e280805216482ad6e73bb3f513aaf786
GanXiKang <gan0612xk@hotmail.com>
2023-10-07 03:28:07 +0800

    feat ： 完整路綫

--- Assets/Script/MiniGame3/MG3_Cup3Control.cs
+++ Assets/Script/MiniGame3/MG3_Cup3Control.cs
@@ -223,6 +223,86 @@ public class MG3_Cup3Control : MonoBehaviour
             yield return null;
         }
         transform.position = targetA.position;
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetB.position, t) + new Vector3(0, 0, 2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetB.position;
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetA.position, t) + new Vector3(0, 0, -2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetA.position;
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetB.position, t) + new Vector3(0, 0, 2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetB.position;
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetC.position, t) + new Vector3(0, 0, 2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetC.position;
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetB.position, t) + new Vector3(0, 0, -2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetB.position;
     }
 
     private void OnTriggerEnter(Collider other)
