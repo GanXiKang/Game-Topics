8c7b2b05624d062ffd7021217dd9016b6b9fba7b
GanXiKang <gan0612xk@hotmail.com>
2023-10-06 23:34:29 +0800

    feat : 清空

--- Assets/Script/MiniGame3/MG3_ButtonControl.cs
+++ Assets/Script/MiniGame3/MG3_ButtonControl.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class MG3_ButtonControl : MonoBehaviour
 { 
-    public GameObject startUI, B_Next, B_Box1, B_Box2, B_Box3;
+    public GameObject startUI, B_Next, B_Cup1, B_Cup2, B_Cup3;
     public Text score;
     public AudioSource BGM;
     public AudioClip button, openCup, closeCup;
@@ -17,62 +17,19 @@ public class MG3_ButtonControl : MonoBehaviour
 
     public void Button_Start()
     {
-        //BGM.PlayOneShot(button);
-        //startUI.SetActive(false);
-        //B_Box1.gameObject.SetActive(true);
-        //B_Box2.gameObject.SetActive(true);
-        //B_Box3.gameObject.SetActive(true);
-        //MG3_StarControl.insStar = true;
-        //downCup = true;
-        //BGM.PlayOneShot(closeCup);
-        //destoryStarNum++;
-        //round++;
-        //print("Round" + round);
+        
     }
     public void NextRound()
     {
-        //B_Next.gameObject.SetActive(false);
-        //B_Box1.gameObject.SetActive(true);
-        //B_Box2.gameObject.SetActive(true);
-        //B_Box3.gameObject.SetActive(true);
-        //MG3_StarControl.insStar = true;
-        //downCup = true;
-        //BGM.PlayOneShot(closeCup);
-        //destoryStar = true;
-        //destoryStarNum++;
-        //round++;
-        //print("Round" + round);
+        
     }
     public void OpenWhyCup(int i)
     {
-        //MG3_BoxMoveControl.openBox1 = true;
-        openBox = true;
-        openWhyBox = i;
-        B_Box1.gameObject.SetActive(false);
-        B_Box2.gameObject.SetActive(false);
-        B_Box3.gameObject.SetActive(false);
-        startActive = true;
-        BGM.PlayOneShot(openCup);
+        
     }
     
     void Update()
     {
-        if (round <= 3)
-        {
-            if (startActive == true)
-            {
-                startActive = false;
-                StartCoroutine(StartSetActice());
-            }
-            score.text = " " + MG3_StarControl.win;
-        }
-    }
-    IEnumerator StartSetActice()
-    {
-        yield return new WaitForSeconds(2.5f);
-        if (round < 3)
-        {
-            B_Next.gameObject.SetActive(true);
-        }
+
     }
 }
