20f336cd872bb4fca69f7908684e2c820603003f
GanXiKang <gan0612xk@hotmail.com>
2023-10-06 23:46:48 +0800

    feat : 編寫code

--- Assets/Script/MiniGame3/MG3_GameControl.cs
+++ Assets/Script/MiniGame3/MG3_GameControl.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class MG3_GameControl : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public static int round;
+    public static bool isMove = false;
+    public static float duration;
 
-    
     void Update()
     {
-        
+        switch (round)
+        {
+            case 1:
+                duration = 2f;
+                break;
+
+            case 2:
+                duration = 1f;
+                break;
+
+            case 3:
+                duration = 0.5f;
+                break;
+        }
     }
 }
