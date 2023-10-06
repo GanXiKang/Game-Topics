beece516de80fad36b1c4941053db76fbba0715b
GanXiKang <gan0612xk@hotmail.com>
2023-10-07 02:55:35 +0800

    feat ： 完整round2的路綫

--- Assets/Script/MiniGame3/MG3_Cup1Control.cs
+++ Assets/Script/MiniGame3/MG3_Cup1Control.cs
@@ -134,6 +134,26 @@ public class MG3_Cup1Control : MonoBehaviour
         }
         transform.position = targetC.position;
 
+        yield return new WaitForSeconds(MG3_GameControl.duration);
+
+        yield return new WaitForSeconds(MG3_GameControl.duration);
+
+        startPosition = transform.position;
+        elapsedTime = 0;
+        while (elapsedTime < MG3_GameControl.duration)
+        {
+            float t = elapsedTime / MG3_GameControl.duration;
+
+            float sinValue = Mathf.Sin(t * Mathf.PI);
+            float y = Mathf.Lerp(0, 2, (sinValue + 1) / 2);
+
+            transform.position = Vector3.Lerp(startPosition, targetA.position, t) + new Vector3(0, 0, -2) * y;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetA.position;
+
         isNewRound = true;
     }
     private IEnumerator MoveSemiCircleRound3()
