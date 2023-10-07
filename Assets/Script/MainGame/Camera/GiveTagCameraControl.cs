cf3f6ec5492dff5bef257f64783720b2d34adef8
GanXiKang <gan0612xk@hotmail.com>
2023-10-08 00:50:02 +0800

    feat ： 編寫全動物的程式碼

--- Assets/Script/MainGame/Camera/GiveTagCameraControl.cs
+++ Assets/Script/MainGame/Camera/GiveTagCameraControl.cs
@@ -85,5 +85,165 @@ public class GiveTagCameraControl : MonoBehaviour
                 gameObject.tag = "P4Camera";
             }
         }
+
+        else if (this.gameObject.tag == "Dragon")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 5)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 5)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 5)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 5)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Snake")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 6)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 6)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 6)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 6)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Horse")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 7)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 7)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 7)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 7)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Sheep")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 8)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 8)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 8)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 8)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Monkey")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 9)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 9)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 9)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 9)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Chinken")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 10)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 10)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 10)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 10)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Dog")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 11)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 11)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 11)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 11)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
+
+        else if (this.gameObject.tag == "Pig")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 12)
+            {
+                gameObject.tag = "P1Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 12)
+            {
+                gameObject.tag = "P2Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 12)
+            {
+                gameObject.tag = "P3Camera";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 12)
+            {
+                gameObject.tag = "P4Camera";
+            }
+        }
     }
 }
