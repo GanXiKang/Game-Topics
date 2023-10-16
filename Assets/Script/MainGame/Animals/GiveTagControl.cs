34a1dff5619bf5524edb7b76902629d9079e215a
GanXiKang <gan0612xk@hotmail.com>
2023-10-17 02:00:14 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/Animals/GiveTagControl.cs
+++ Assets/Script/MainGame/Animals/GiveTagControl.cs
@@ -85,5 +85,165 @@ public class GiveTagControl : MonoBehaviour
                 gameObject.tag = "P4";
             }
         }
+
+        else if (gameObject.tag == "Dragon")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 5)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 5)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 5)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 5)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Snake")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 6)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 6)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 6)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 6)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Horse")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 7)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 7)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 7)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 7)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Sheep")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 8)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 8)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 8)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 8)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Monkey")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 9)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 9)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 9)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 9)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Chicken")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 10)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 10)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 10)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 10)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Dog")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 11)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 11)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 11)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 11)
+            {
+                gameObject.tag = "P4";
+            }
+        }
+
+        else if (gameObject.tag == "Pig")
+        {
+            if (Menu_ChoosePlayer.whyP1 == 12)
+            {
+                gameObject.tag = "P1";
+            }
+            else if (Menu_ChoosePlayer.whyP2 == 12)
+            {
+                gameObject.tag = "P2";
+            }
+            else if (Menu_ChoosePlayer.whyP3 == 12)
+            {
+                gameObject.tag = "P3";
+            }
+            else if (Menu_ChoosePlayer.whyP4 == 12)
+            {
+                gameObject.tag = "P4";
+            }
+        }
     }
 }
