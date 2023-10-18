ee69e406ea41980d427654decc76800a532d8362
GanXiKang <gan0612xk@hotmail.com>
2023-10-19 02:23:36 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/Audio/AudioControl.cs
+++ Assets/Script/MainGame/Audio/AudioControl.cs
@@ -26,5 +26,41 @@ public class AudioControl : MonoBehaviour
             BGM.PlayOneShot(destoryKickboard);
             InsKickboardControl.destoryKickboard = false;
         }
+        if (MiniGameColliderControl.isRacoonCall || MGFinishAwardControl.isRacoonCall)
+        {
+            BGM.PlayOneShot(racoon);
+            MiniGameColliderControl.isRacoonCall = false;
+            MGFinishAwardControl.isRacoonCall = false;
+        }
+        if (MiniGameColliderControl.isSlothCall || MGFinishAwardControl.isSlothCall)
+        {
+            BGM.PlayOneShot(sloth);
+            MiniGameColliderControl.isSlothCall = false;
+            MGFinishAwardControl.isSlothCall = false;
+        }
+        if (MiniGameColliderControl.isFoxCall || MGFinishAwardControl.isFoxCall)
+        {
+            BGM.PlayOneShot(fox);
+            MiniGameColliderControl.isFoxCall = false;
+            MGFinishAwardControl.isFoxCall = false;
+        }
+        if (MiniGameColliderControl.isOstrichCall || MGFinishAwardControl.isOstrichCall)
+        {
+            BGM.PlayOneShot(ostrich);
+            MiniGameColliderControl.isOstrichCall = false;
+            MGFinishAwardControl.isOstrichCall = false;
+        }
+        if (MiniGameColliderControl.isBearCall || MGFinishAwardControl.isBearCall)
+        {
+            BGM.PlayOneShot(bear);
+            MiniGameColliderControl.isBearCall = false;
+            MGFinishAwardControl.isBearCall = false;
+        }
+        if (MiniGameColliderControl.isDolphinCall || MGFinishAwardControl.isDolphinCall)
+        {
+            BGM.PlayOneShot(dolphin);
+            MiniGameColliderControl.isDolphinCall = false;
+            MGFinishAwardControl.isDolphinCall = false;
+        }
     }
 }
