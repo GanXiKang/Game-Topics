e0a570476ae1a6e6bbcfbe0f3c881c23ad3d3782
GanXiKang <gan0612xk@hotmail.com>
2023-10-20 00:00:07 +0800

    feat ： new script

---
+++ Assets/Script/MainGame/Animals/AnimalsExpressionControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalsExpressionControl : MonoBehaviour
+{
+    SkinnedMeshRenderer smr;
+
+    public Material[] expression = new Material[4];
+
+    bool isBlink;
+
+    void Start()
+    {
+        smr = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (isBlink)
+        {
+            StartCoroutine(AnimalsBlink());
+        }
+    }
+    
+    IEnumerator AnimalsBlink()
+    {
+        isBlink = false;
+        smr.material = expression[1];
+        yield return new WaitForSeconds(0.1f);
+        smr.material = expression[2];
+        yield return new WaitForSeconds(0.1f);
+        smr.material = expression[3];
+        yield return new WaitForSeconds(0.1f);
+        smr.material = expression[2];
+        yield return new WaitForSeconds(0.1f);
+        smr.material = expression[1];
+        yield return new WaitForSeconds(0.1f);
+        smr.material = expression[0];
+        yield return new WaitForSeconds(10f);
+        isBlink = true;
+
+    }
+}
