d1c610ac461b4da28cc519ec7e1f33e1a4337f39
GanXiKang <gan0612xk@hotmail.com>
2023-10-24 01:17:49 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/Collider/StoreColliderControl.cs
+++ Assets/Script/MainGame/Collider/StoreColliderControl.cs
@@ -24,9 +24,16 @@ public class StoreColliderControl : MonoBehaviour
                         who = 1;
                         P1_EnterStore = false;
                         AnimatorControl.isP1Move = false;
-                        AnimatorControl.isP1Skill = false;
-                        AnimatorControl.isP1Wave = true;
-                        StartCoroutine(LookTarget());
+                        if (!AnimalsPowerControl.dragonUsePower || Menu_ChoosePlayer.whyP1 != 5)
+                        {
+                            AnimatorControl.isP1Skill = false;
+                            AnimatorControl.isP1Wave = true;
+                            StartCoroutine(LookTarget());
+                        }
+                        else
+                        {
+                            StartCoroutine(DragonFlyNow());
+                        }
                         if (Menu_ChoosePlayer.whyP1 == 1)
                         {
                             PowerUIControl.isMouseCanUsePower = true;
@@ -41,9 +48,16 @@ public class StoreColliderControl : MonoBehaviour
                         who = 2;
                         P2_EnterStore = false;
                         AnimatorControl.isP2Move = false;
-                        AnimatorControl.isP2Skill = false;
-                        AnimatorControl.isP2Wave = true;
-                        StartCoroutine(LookTarget());
+                        if (!AnimalsPowerControl.dragonUsePower || Menu_ChoosePlayer.whyP2 != 5)
+                        {
+                            AnimatorControl.isP2Skill = false;
+                            AnimatorControl.isP2Wave = true;
+                            StartCoroutine(LookTarget());
+                        }
+                        else
+                        {
+                            StartCoroutine(DragonFlyNow());
+                        }
                         if (Menu_ChoosePlayer.whyP2 == 1)
                         {
                             PowerUIControl.isMouseCanUsePower = true;
@@ -58,9 +72,16 @@ public class StoreColliderControl : MonoBehaviour
                         who = 3;
                         P3_EnterStore = false;
                         AnimatorControl.isP3Move = false;
-                        AnimatorControl.isP3Skill = false;
-                        AnimatorControl.isP3Wave = true;
-                        StartCoroutine(LookTarget());
+                        if (!AnimalsPowerControl.dragonUsePower || Menu_ChoosePlayer.whyP3 != 5)
+                        {
+                            AnimatorControl.isP3Skill = false;
+                            AnimatorControl.isP3Wave = true;
+                            StartCoroutine(LookTarget());
+                        }
+                        else
+                        {
+                            StartCoroutine(DragonFlyNow());
+                        }
                         if (Menu_ChoosePlayer.whyP3 == 1)
                         {
                             PowerUIControl.isMouseCanUsePower = true;
@@ -75,9 +96,16 @@ public class StoreColliderControl : MonoBehaviour
                         who = 4;
                         P4_EnterStore = false;
                         AnimatorControl.isP4Move = false;
-                        AnimatorControl.isP4Skill = false;
-                        AnimatorControl.isP4Wave = true;
-                        StartCoroutine(LookTarget());
+                        if (!AnimalsPowerControl.dragonUsePower || Menu_ChoosePlayer.whyP4 != 5)
+                        {
+                            AnimatorControl.isP4Skill = false;
+                            AnimatorControl.isP4Wave = true;
+                            StartCoroutine(LookTarget());
+                        }
+                        else
+                        {
+                            StartCoroutine(DragonFlyNow());
+                        }
                         if (Menu_ChoosePlayer.whyP4 == 1)
                         {
                             PowerUIControl.isMouseCanUsePower = true;
@@ -206,6 +234,16 @@ public class StoreColliderControl : MonoBehaviour
         StoreUIControl.isStore = true;
         isStoreNotStopUI = true;
         FalseByAnimator();
+    }
+    IEnumerator DragonFlyNow()
+    {
+        AnimalsPowerControl.dragonPowerRound++;
+        AnimatorControl.isDragonFlyWalk = false;
+        CameraMoveControl.isChangeCameraPoint = true;
+        yield return new WaitForSeconds(2f);
+        ChangeCameraControl.changeCameraNum++;
+        DiceUIControl.isDiceUI = true;
+        CameraMoveControl.isChangeCameraPoint = false;
     }
     IEnumerator Transposition()
     {
