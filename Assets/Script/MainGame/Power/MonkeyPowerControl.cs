3275c744ac7420f38f66a22ea4d52d72a97a00a9
GanXiKang <gan0612xk@hotmail.com>
2023-10-24 01:57:42 +0800

    feat : 更改内容

--- Assets/Script/MainGame/Power/MonkeyPowerControl.cs
+++ Assets/Script/MainGame/Power/MonkeyPowerControl.cs
@@ -10,47 +10,47 @@ public class MonkeyPowerControl : MonoBehaviour
 
     void Update()
     {
-        //if (BagUIControl.isBomb)
-        //{
-        //    switch (ChangeCameraControl.changeCameraNum)
-        //    {
-        //        case 1:
-        //            if (gameObject.tag == "P1")
-        //            {
-        //                isP1InsBanana = true;
-        //                BombPropsControl.pointNum = DiceControl.P1_totalNum;
-        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
-        //            }
-        //            break;
+        if (BagUIControl.isBomb)
+        {
+            switch (ChangeCameraControl.changeCameraNum)
+            {
+                case 1:
+                    if (gameObject.tag == "P1")
+                    {
+                        isP1InsBanana = true;
+                        BananaControl.pointNum = DiceControl.P1_totalNum;
+                        Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+                    }
+                    break;
 
-        //        case 2:
-        //            if (gameObject.tag == "P2")
-        //            {
-        //                isP2InsBanana = true;
-        //                BombPropsControl.pointNum = DiceControl.P2_totalNum;
-        //                Instantiate(banana, insBananaPoint.position, insbananaPoint.rotation);
-        //            }
-        //            break;
+                case 2:
+                    if (gameObject.tag == "P2")
+                    {
+                        isP2InsBanana = true;
+                        BananaControl.pointNum = DiceControl.P2_totalNum;
+                        Instantiate(banana, insBananaPoint.position, insbananaPoint.rotation);
+                    }
+                    break;
 
-        //        case 3:
-        //            if (gameObject.tag == "P3")
-        //            {
-        //                isP3InsBanana = true;
-        //                BombPropsControl.pointNum = DiceControl.P3_totalNum;
-        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
-        //            }
-        //            break;
+                case 3:
+                    if (gameObject.tag == "P3")
+                    {
+                        isP3InsBanana = true;
+                        BananaControl.pointNum = DiceControl.P3_totalNum;
+                        Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+                    }
+                    break;
 
-        //        case 4:
-        //            if (gameObject.tag == "P4")
-        //            {
-        //                isP4InsBanana = true;
-        //                BombPropsControl.pointNum = DiceControl.P4_totalNum;
-        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
-        //            }
-        //            break;
-        //    }
-        //    BagUIControl.isBomb = false;
-        //}
+                case 4:
+                    if (gameObject.tag == "P4")
+                    {
+                        isP4InsBanana = true;
+                        BananaControl.pointNum = DiceControl.P4_totalNum;
+                        Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+                    }
+                    break;
+            }
+            //BagUIControl.isBomb = false;
+        }
     }
 }
