5d0552b477e69ca8b988531f1e10b3800b116092
GanXiKang <gan0612xk@hotmail.com>
2023-10-24 01:52:07 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/Power/MonkeyPowerControl.cs
+++ Assets/Script/MainGame/Power/MonkeyPowerControl.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class MonkeyPowerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject banana;
+    public Transform insBananaPoint;
+    public static bool isP1InsBanana = false, isP2InsBanana = false, isP3InsBanana = false, isP4InsBanana = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //if (BagUIControl.isBomb)
+        //{
+        //    switch (ChangeCameraControl.changeCameraNum)
+        //    {
+        //        case 1:
+        //            if (gameObject.tag == "P1")
+        //            {
+        //                isP1InsBanana = true;
+        //                BombPropsControl.pointNum = DiceControl.P1_totalNum;
+        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+        //            }
+        //            break;
+
+        //        case 2:
+        //            if (gameObject.tag == "P2")
+        //            {
+        //                isP2InsBanana = true;
+        //                BombPropsControl.pointNum = DiceControl.P2_totalNum;
+        //                Instantiate(banana, insBananaPoint.position, insbananaPoint.rotation);
+        //            }
+        //            break;
+
+        //        case 3:
+        //            if (gameObject.tag == "P3")
+        //            {
+        //                isP3InsBanana = true;
+        //                BombPropsControl.pointNum = DiceControl.P3_totalNum;
+        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+        //            }
+        //            break;
+
+        //        case 4:
+        //            if (gameObject.tag == "P4")
+        //            {
+        //                isP4InsBanana = true;
+        //                BombPropsControl.pointNum = DiceControl.P4_totalNum;
+        //                Instantiate(banana, insBananaPoint.position, insBananaPoint.rotation);
+        //            }
+        //            break;
+        //    }
+        //    BagUIControl.isBomb = false;
+        //}
     }
 }
