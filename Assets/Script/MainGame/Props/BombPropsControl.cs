8db06f7c42c59d75b95fc67ceb63fff0b5a81d03
GanXiKang <gan0612xk@hotmail.com>
2023-10-25 02:38:37 +0800

    feat ： 編寫雞power

--- Assets/Script/MainGame/Props/BombPropsControl.cs
+++ Assets/Script/MainGame/Props/BombPropsControl.cs
@@ -31,13 +31,20 @@ public class BombPropsControl : MonoBehaviour
             {
                 if (DiceControl.P1_totalNum == pointNum)
                 {
-                    AnimatorControl.isP1Dizziness = true;
-                    IsStopUIControl.isBombStopP1 = true;
-                    StartCoroutine(StopTiming());
-                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP1 == 5)
+                    if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP1 != 10)
                     {
-                        AnimalsPowerControl.dragonUsePower = false;
-                        AnimatorControl.isP1Skill = false;
+                        AnimatorControl.isP1Dizziness = true;
+                        IsStopUIControl.isBombStopP1 = true;
+                        StartCoroutine(StopTiming());
+                        if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP1 == 5)
+                        {
+                            AnimalsPowerControl.dragonUsePower = false;
+                            AnimatorControl.isP1Skill = false;
+                        }
+                    }
+                    else
+                    {
+                        Destroy(this.gameObject);
                     }
                 }
             }
@@ -52,13 +59,20 @@ public class BombPropsControl : MonoBehaviour
             {
                 if (DiceControl.P2_totalNum == pointNum)
                 {
-                    AnimatorControl.isP2Dizziness = true;
-                    IsStopUIControl.isBombStopP2 = true;
-                    StartCoroutine(StopTiming());
-                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP2 == 5)
+                    if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP2 != 10)
                     {
-                        AnimalsPowerControl.dragonUsePower = false;
-                        AnimatorControl.isP2Skill = false;
+                        AnimatorControl.isP2Dizziness = true;
+                        IsStopUIControl.isBombStopP2 = true;
+                        StartCoroutine(StopTiming());
+                        if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP2 == 5)
+                        {
+                            AnimalsPowerControl.dragonUsePower = false;
+                            AnimatorControl.isP2Skill = false;
+                        }
+                    }
+                    else
+                    {
+                        Destroy(this.gameObject);
                     }
                 }
             }
@@ -73,13 +87,20 @@ public class BombPropsControl : MonoBehaviour
             {
                 if (DiceControl.P3_totalNum == pointNum)
                 {
-                    AnimatorControl.isP3Dizziness = true;
-                    IsStopUIControl.isBombStopP3 = true;
-                    StartCoroutine(StopTiming());
-                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP3 == 5)
+                    if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP3 != 10)
                     {
-                        AnimalsPowerControl.dragonUsePower = false;
-                        AnimatorControl.isP3Skill = false;
+                        AnimatorControl.isP3Dizziness = true;
+                        IsStopUIControl.isBombStopP3 = true;
+                        StartCoroutine(StopTiming());
+                        if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP3 == 5)
+                        {
+                            AnimalsPowerControl.dragonUsePower = false;
+                            AnimatorControl.isP3Skill = false;
+                        }
+                    }
+                    else
+                    {
+                        Destroy(this.gameObject);
                     }
                 }
             }
@@ -94,13 +115,20 @@ public class BombPropsControl : MonoBehaviour
             {
                 if (DiceControl.P4_totalNum == pointNum)
                 {
-                    AnimatorControl.isP4Dizziness = true;
-                    IsStopUIControl.isBombStopP4 = true;
-                    StartCoroutine(StopTiming());
-                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP4 == 5)
+                    if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP4 != 10)
                     {
-                        AnimalsPowerControl.dragonUsePower = false;
-                        AnimatorControl.isP4Skill = false;
+                        AnimatorControl.isP4Dizziness = true;
+                        IsStopUIControl.isBombStopP4 = true;
+                        StartCoroutine(StopTiming());
+                        if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP4 == 5)
+                        {
+                            AnimalsPowerControl.dragonUsePower = false;
+                            AnimatorControl.isP4Skill = false;
+                        }
+                    }
+                    else
+                    {
+                        Destroy(this.gameObject);
                     }
                 }
             }
