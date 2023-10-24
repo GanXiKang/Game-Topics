93d19aabb46cde76b018f97dd5000cd29920f967
GanXiKang <gan0612xk@hotmail.com>
2023-10-25 03:10:08 +0800

    feat ： 添加print

--- Assets/Script/MainGame/Camera/ChangeCameraControl.cs
+++ Assets/Script/MainGame/Camera/ChangeCameraControl.cs
@@ -16,6 +16,7 @@ public class ChangeCameraControl : MonoBehaviour
             if (AnimalsPowerControl.chickenUsePower)
             {
                 AnimalsPowerControl.chickenPowerRound += 1 ;
+                print(AnimalsPowerControl.chickenPowerRound);
             }
         }
     }
