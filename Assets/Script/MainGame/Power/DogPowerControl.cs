7f3e7ff64c672ab8944b339a1a7ab6b1f7bf1086
GanXiKang <gan0612xk@hotmail.com>
2023-10-25 23:32:19 +0800

    feat : new Script

---
+++ Assets/Script/MainGame/Power/DogPowerControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogPowerControl : MonoBehaviour
+{
+    BoxCollider bc;
+
+    public 
+    void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
