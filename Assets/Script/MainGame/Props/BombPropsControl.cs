bd42abe855609d44a7c4825e8c9fdd38b239f7f9
GanXiKang <gan0612xk@hotmail.com>
2023-11-09 02:55:52 +0800

    feat ： 調整時間

--- Assets/Script/MainGame/Props/BombPropsControl.cs
+++ Assets/Script/MainGame/Props/BombPropsControl.cs
@@ -156,89 +156,97 @@ public class BombPropsControl : MonoBehaviour
                 switch (ChangeCameraControl.changeCameraNum)
                 {
                     case 1:
-                        if (DiceControl.P1_totalNum == pointNum)
+                        if (other.tag == "P1")
                         {
-                            if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP1 != 10)
+                            if (DiceControl.P1_totalNum == pointNum)
                             {
-                                AnimatorControl.isP1Dizziness = true;
-                                IsStopUIControl.isBombStopP1 = true;
-                                StartCoroutine(StopTiming());
-                                if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP1 == 5)
+                                if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP1 != 10)
                                 {
-                                    AnimalsPowerControl.dragonUsePower = false;
-                                    AnimatorControl.isP1Skill = false;
+                                    AnimatorControl.isP1Dizziness = true;
+                                    IsStopUIControl.isBombStopP1 = true;
+                                    StartCoroutine(StopTiming());
+                                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP1 == 5)
+                                    {
+                                        AnimalsPowerControl.dragonUsePower = false;
+                                        AnimatorControl.isP1Skill = false;
+                                    }
                                 }
-                                once = false;
-                            }
-                            else
-                            {
-                                Destroy(this.gameObject);
+                                else
+                                {
+                                    Destroy(this.gameObject);
+                                }
                             }
                         }
                         break;
 
                     case 2:
-                        if (DiceControl.P2_totalNum == pointNum)
+                        if (other.tag == "P2")
                         {
-                            if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP2 != 10)
+                            if (DiceControl.P2_totalNum == pointNum)
                             {
-                                AnimatorControl.isP2Dizziness = true;
-                                IsStopUIControl.isBombStopP2 = true;
-                                StartCoroutine(StopTiming());
-                                if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP2 == 5)
+                                if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP2 != 10)
                                 {
-                                    AnimalsPowerControl.dragonUsePower = false;
-                                    AnimatorControl.isP2Skill = false;
+                                    AnimatorControl.isP2Dizziness = true;
+                                    IsStopUIControl.isBombStopP2 = true;
+                                    StartCoroutine(StopTiming());
+                                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP2 == 5)
+                                    {
+                                        AnimalsPowerControl.dragonUsePower = false;
+                                        AnimatorControl.isP2Skill = false;
+                                    }
                                 }
-                                once = false;
-                            }
-                            else
-                            {
-                                Destroy(this.gameObject);
+                                else
+                                {
+                                    Destroy(this.gameObject);
+                                }
                             }
                         }
                         break;
 
                     case 3:
-                        if (DiceControl.P3_totalNum == pointNum)
+                        if (other.tag == "P3")
                         {
-                            if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP3 != 10)
+                            if (DiceControl.P3_totalNum == pointNum)
                             {
-                                AnimatorControl.isP3Dizziness = true;
-                                IsStopUIControl.isBombStopP3 = true;
-                                StartCoroutine(StopTiming());
-                                if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP3 == 5)
+                                if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP3 != 10)
                                 {
-                                    AnimalsPowerControl.dragonUsePower = false;
-                                    AnimatorControl.isP3Skill = false;
+                                    AnimatorControl.isP3Dizziness = true;
+                                    IsStopUIControl.isBombStopP3 = true;
+                                    StartCoroutine(StopTiming());
+                                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP3 == 5)
+                                    {
+                                        AnimalsPowerControl.dragonUsePower = false;
+                                        AnimatorControl.isP3Skill = false;
+                                    }
                                 }
-                                once = false;
-                            }
-                            else
-                            {
-                                Destroy(this.gameObject);
+                                else
+                                {
+                                    Destroy(this.gameObject);
+                                }
                             }
                         }
                         break;
 
                     case 4:
-                        if (DiceControl.P4_totalNum == pointNum)
+                        if (other.tag == "P4")
                         {
-                            if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP4 != 10)
+                            if (DiceControl.P4_totalNum == pointNum)
                             {
-                                AnimatorControl.isP4Dizziness = true;
-                                IsStopUIControl.isBombStopP4 = true;
-                                StartCoroutine(StopTiming());
-                                if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP4 == 5)
+                                if (!AnimalsPowerControl.chickenUsePower || Menu_ChoosePlayer.whyP4 != 10)
                                 {
-                                    AnimalsPowerControl.dragonUsePower = false;
-                                    AnimatorControl.isP4Skill = false;
+                                    AnimatorControl.isP4Dizziness = true;
+                                    IsStopUIControl.isBombStopP4 = true;
+                                    StartCoroutine(StopTiming());
+                                    if (AnimalsPowerControl.dragonUsePower && Menu_ChoosePlayer.whyP4 == 5)
+                                    {
+                                        AnimalsPowerControl.dragonUsePower = false;
+                                        AnimatorControl.isP4Skill = false;
+                                    }
                                 }
-                                once = false;
-                            }
-                            else
-                            {
-                                Destroy(this.gameObject);
+                                else
+                                {
+                                    Destroy(this.gameObject);
+                                }
                             }
                         }
                         break;
@@ -250,14 +258,15 @@ public class BombPropsControl : MonoBehaviour
     IEnumerator StopTiming()
     {
         r = false;
+        once = false;
         rb.isKinematic = false;
         boom.Play();
         iscolliderBombText = true;
         DiceUIControl.isDiceUI = false;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(4f);
         ChangeCameraControl.changeCameraNum++;
         DiceUIControl.isDiceUI = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         IsStopUIControl.isBombStopUI++;
         Destroy(this.gameObject);
     }
