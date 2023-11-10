b99bf86a5cd939d938a0680304b3bef1229c9967
GanXiKang <gan0612xk@hotmail.com>
2023-11-11 02:01:15 +0800

    feat ： 編寫變數等

--- Assets/Script/MainGame/Props/InsPropsControl.cs
+++ Assets/Script/MainGame/Props/InsPropsControl.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class InsPropsControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] props = new GameObject[6];
+    public Transform insPropsPoint;
 
-    // Update is called once per frame
     void Update()
     {
         
