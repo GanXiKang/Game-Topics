e1ce8c68a188fd3c7b4ab32d85fd4c3b2f375f81
GanXiKang <gan0612xk@hotmail.com>
2023-11-11 02:28:11 +0800

    feat ： 編寫code

--- Assets/Script/MainGame/Animals/InsPropsControl.cs
+++ Assets/Script/MainGame/Animals/InsPropsControl.cs
@@ -7,12 +7,48 @@ public class InsPropsControl : MonoBehaviour
     public GameObject[] props = new GameObject[6];
     public Transform insPropsPoint;
 
+    public static bool[] isInsprops;
+
     void Start()
     {
-        Instantiate(props[0], insPropsPoint.position, insPropsPoint.rotation);
+        isInsprops[0] = false;
+        isInsprops[1] = false;
+        isInsprops[2] = false;
+        isInsprops[3] = false;
+        isInsprops[4] = false;
+        isInsprops[5] = false;
     }
     void Update()
     {
-        
+        if (isInsprops[0])
+        {
+            Instantiate(props[0], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[0] = false;
+        }
+        else if (isInsprops[1])
+        {
+            Instantiate(props[1], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[1] = false;
+        }
+        else if (isInsprops[2])
+        {
+            Instantiate(props[2], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[2] = false;
+        }
+        else if (isInsprops[3])
+        {
+            Instantiate(props[3], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[3] = false;
+        }
+        else if (isInsprops[4])
+        {
+            Instantiate(props[4], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[4] = false;
+        }
+        else if (isInsprops[5])
+        {
+            Instantiate(props[5], insPropsPoint.position, insPropsPoint.rotation);
+            isInsprops[5] = false;
+        }
     }
 }
