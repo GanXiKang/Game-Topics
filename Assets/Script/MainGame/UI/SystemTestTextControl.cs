40bfeef5bed17badb7e96768a70900795c7caa32
GanXiKang <gan0612xk@hotmail.com>
2023-11-17 00:51:46 +0800

    feat ： 字體變回去

--- Assets/Script/MainGame/UI/SystemTestTextControl.cs
+++ Assets/Script/MainGame/UI/SystemTestTextControl.cs
@@ -8,6 +8,7 @@ public class SystemTestTextControl : MonoBehaviour
     float timer = 0;
 
     public GameObject backGround;
+    public Text systemText;
 
     public static bool isTimer = false;
 
@@ -17,6 +18,7 @@ public class SystemTestTextControl : MonoBehaviour
         if (timer > 2f)
         {
             backGround.SetActive(false);
+            systemText.fontSize = 18;
         }
 
         if (isTimer)
