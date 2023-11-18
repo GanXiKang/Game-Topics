8b8151ac413fceb1629c2d5cba75437ba89302a8
GanXiKang <gan0612xk@hotmail.com>
2023-11-19 04:11:01 +0800

    feat : 編寫code

--- Assets/Script/MainGame/Camera/StartCameraControl.cs
+++ Assets/Script/MainGame/Camera/StartCameraControl.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class StartCameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] c = new GameObject[7]; 
+
     void Start()
     {
-        
+        StartCoroutine(OpeningShow());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator OpeningShow()
     {
-        
+        c[7].SetActive(true);
+        DiceUIControl.isDiceUI = false;
+        yield return new WaitForSeconds(1f);
+        c[7].SetActive(false);
+        c[6].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[6].SetActive(false);
+        c[5].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[5].SetActive(false);
+        c[4].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[4].SetActive(false);
+        c[3].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[3].SetActive(false);
+        c[2].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[2].SetActive(false);
+        c[1].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        c[1].SetActive(false);
+        c[0].SetActive(true);
+        yield return new WaitForSeconds(1.3f);
+        c[0].SetActive(false);
+        DiceUIControl.isDiceUI = true;
     }
 }
