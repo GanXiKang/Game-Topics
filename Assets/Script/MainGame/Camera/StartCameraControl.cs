af601586bc00cd1e1de38ff50a7beaa017dd642d
GanXiKang <gan0612xk@hotmail.com>
2023-11-19 04:31:04 +0800

    feat ： 添加動作

--- Assets/Script/MainGame/Camera/StartCameraControl.cs
+++ Assets/Script/MainGame/Camera/StartCameraControl.cs
@@ -34,8 +34,16 @@ public class StartCameraControl : MonoBehaviour
         yield return new WaitForSeconds(1f);
         c[1].SetActive(false);
         c[0].SetActive(true);
+        AnimatorControl.isP1Wave = true;
+        AnimatorControl.isP2Wave = true;
+        AnimatorControl.isP3Wave = true;
+        AnimatorControl.isP4Wave = true;
         yield return new WaitForSeconds(1.3f);
         c[0].SetActive(false);
         DiceUIControl.isDiceUI = true;
+        AnimatorControl.isP1Wave = false;
+        AnimatorControl.isP2Wave = false;
+        AnimatorControl.isP3Wave = false;
+        AnimatorControl.isP4Wave = false;
     }
 }
