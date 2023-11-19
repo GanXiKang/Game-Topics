11d1e780224b160422261751d59a71c36e01d184
GanXiKang <gan0612xk@hotmail.com>
2023-11-20 03:02:15 +0800

    feat : 編寫code

--- Assets/Script/MainGame/Effects/RunEffectsControl.cs
+++ Assets/Script/MainGame/Effects/RunEffectsControl.cs
@@ -4,11 +4,51 @@ using UnityEngine;
 
 public class RunEffectsControl : MonoBehaviour
 {
+    GameObject cow, rabbit, horse, dog;
+
+    void Start()
+    {
+        if (AnimalsPowerControl.cowUsePower)
+        {
+            cow = GameObject.Find("Cow(Clone)");
+        }
+        if (AnimalsPowerControl.rabbitUsePower)
+        {
+            rabbit = GameObject.Find("Rabbit(Clone)");
+        }
+        if (AnimalsPowerControl.horseUsePower)
+        {
+            horse = GameObject.Find("Horse(Clone)");
+        }
+        if (AnimalsPowerControl.dogUsePower)
+        {
+            dog = GameObject.Find("Dog(Clone)");
+        }
+    }
     void Update()
     {
         if (!AnimalsPowerControl.cowUsePower && !AnimalsPowerControl.rabbitUsePower && !AnimalsPowerControl.horseUsePower && !AnimalsPowerControl.dogUsePower)
         {
             Destroy(gameObject);
+        }
+        else
+        {
+            if (AnimalsPowerControl.cowUsePower)
+            {
+                transform.position = new Vector3(cow.transform.position.x, transform.position.y, cow.transform.position.z);
+            }
+            if (AnimalsPowerControl.rabbitUsePower)
+            {
+
+            }
+            if (AnimalsPowerControl.horseUsePower)
+            {
+
+            }
+            if (AnimalsPowerControl.dogUsePower)
+            {
+                transform.position = new Vector3(dog.transform.position.x, transform.position.y, dog.transform.position.z);
+            }
         }
     }
 }
