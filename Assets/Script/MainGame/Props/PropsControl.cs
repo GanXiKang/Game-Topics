4f0926c0e98c5795b44bdef308675839de9ccf4a
GanXiKang <gan0612xk@hotmail.com>
2023-11-19 20:29:09 +0800

    feat ： 調整道具初始值

--- Assets/Script/MainGame/Props/PropsControl.cs
+++ Assets/Script/MainGame/Props/PropsControl.cs
@@ -875,29 +875,29 @@ public class PropsControl : MonoBehaviour
 
     void Initial()
     {
-        P1Props[0] = 0;
-        P1Props[1] = 0;
+        P1Props[0] = 1;
+        P1Props[1] = 1;
         P1Props[2] = 0;
         P1Props[3] = 0;
         P1Props[4] = 0;
         P1Props[5] = 0;
         P1Props[6] = 0;
-        P2Props[0] = 0;
-        P2Props[1] = 0;
+        P2Props[0] = 1;
+        P2Props[1] = 1;
         P2Props[2] = 0;
         P2Props[3] = 0;
         P2Props[4] = 0;
         P2Props[5] = 0;
         P2Props[6] = 0;
-        P3Props[0] = 0;
-        P3Props[1] = 0;
+        P3Props[0] = 1;
+        P3Props[1] = 1;
         P3Props[2] = 0;
         P3Props[3] = 0;
         P3Props[4] = 0;
         P3Props[5] = 0;
         P3Props[6] = 0;
-        P4Props[0] = 0;
-        P4Props[1] = 0;
+        P4Props[0] = 1;
+        P4Props[1] = 1;
         P4Props[2] = 0;
         P4Props[3] = 0;
         P4Props[4] = 0;
