a996b62d089d401e0307da18e717351387f4475b
GanXiKang <gan0612xk@hotmail.com>
2023-11-20 01:51:41 +0800

    feat : newScript (拼錯了Destroy)

---
+++ Assets/Script/MainGame/Effects/DestoryEffectsControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryEffectsControl : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+}
