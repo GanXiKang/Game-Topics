54f194bd02696d2372fdce123eb3216506dea45a
GanXiKang <gan0612xk@hotmail.com>
2023-11-21 03:29:45 +0800

    feat : 編寫code

--- Assets/Script/MainMenu/Menu_TutorialControl.cs
+++ Assets/Script/MainMenu/Menu_TutorialControl.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu_TutorialControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] buttonShort = new GameObject[4];
+    public GameObject[] buttonLong = new GameObject[4];
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Button_Beside(int i)
+    {
+        for (int b = 0; b < 4; b++)
+        {
+            if (b == i)
+            {
+                buttonShort[i].SetActive(false);
+                buttonLong[i].SetActive(true);
+            }
+            else
+            {
+                buttonShort[i].SetActive(true);
+                buttonLong[i].SetActive(false);
+            }
+        }
     }
 }
