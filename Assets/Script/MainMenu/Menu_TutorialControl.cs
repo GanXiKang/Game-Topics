2bd53cf20b74828ca2ee047e33d93c509f3214fc
GanXiKang <gan0612xk@hotmail.com>
2023-11-21 14:24:36 +0800

    feat ： 編寫code

--- Assets/Script/MainMenu/Menu_TutorialControl.cs
+++ Assets/Script/MainMenu/Menu_TutorialControl.cs
@@ -7,7 +7,7 @@ public class Menu_TutorialControl : MonoBehaviour
 {
     public GameObject[] buttonShort = new GameObject[4];
     public GameObject[] buttonLong = new GameObject[4];
-    public GameObject frontPage;
+    public GameObject frontPage, animals, scene, mg, props;
 
     int buttonNum;
     bool isButtonDisable;
@@ -29,6 +29,7 @@ public class Menu_TutorialControl : MonoBehaviour
                     buttonLong[b].SetActive(false);
                 }
             }
+            MenuDisable();
             frontPage.SetActive(false);
             isButtonDisable = false;
         }
@@ -38,5 +39,39 @@ public class Menu_TutorialControl : MonoBehaviour
     {
         buttonNum = i;
         isButtonDisable = true;
+    }
+
+    void MenuDisable()
+    {
+        switch (buttonNum)
+        {
+            case 0:
+                animals.SetActive(true);
+                scene.SetActive(false);
+                mg.SetActive(false);
+                props.SetActive(false);
+                break;
+
+            case 1:
+                animals.SetActive(false);
+                scene.SetActive(true);
+                mg.SetActive(false);
+                props.SetActive(false);
+                break;
+
+            case 2:
+                animals.SetActive(false);
+                scene.SetActive(false);
+                mg.SetActive(true);
+                props.SetActive(false);
+                break;
+
+            case 3:
+                animals.SetActive(false);
+                scene.SetActive(false);
+                mg.SetActive(false);
+                props.SetActive(true);
+                break;
+        }
     }
 }
