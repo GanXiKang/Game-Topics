425465861f41994926648f6634301b0113a2c2a1
GanXiKang <gan0612xk@hotmail.com>
2023-11-21 16:31:21 +0800

    feat : 編寫code

--- Assets/Script/MainMenu/Tutorial_AnimalsControl.cs
+++ Assets/Script/MainMenu/Tutorial_AnimalsControl.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial_AnimalsControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource BGM;
+    public AudioClip onClick;
+    public GameObject menu1, menu2;
+    public Image[] introduce = new Image[20];
+
+    public void Button_Animals(int i)
+    {
+        menu1.SetActive(false);
+        menu2.SetActive(true);
+    }
+    public void Button_Animator()
+    {
+       
+    }
+    public void Button_Call()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Button_Back()
     {
-        
+        menu1.SetActive(true);
+        menu2.SetActive(false);
     }
 }
