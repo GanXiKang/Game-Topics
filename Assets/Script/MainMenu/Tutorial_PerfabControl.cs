47a37d62867ac0cb4a1e3fbbfc41d56d852cfc51
GanXiKang <gan0612xk@hotmail.com>
2023-11-23 00:29:00 +0800

    feat ： new script

---
+++ Assets/Script/MainMenu/Tutorial_PerfabControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_PerfabControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
