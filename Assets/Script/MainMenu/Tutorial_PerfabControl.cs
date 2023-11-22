b130e765cdccdfab66a4ff3a7f0e2db4bd401d2f
GanXiKang <gan0612xk@hotmail.com>
2023-11-23 00:50:17 +0800

    feat ： 編寫code

--- Assets/Script/MainMenu/Tutorial_PerfabControl.cs
+++ Assets/Script/MainMenu/Tutorial_PerfabControl.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Tutorial_PerfabControl : MonoBehaviour
 {
+    Animator anim;
+    AudioSource BGM;
+
     void Start()
     {
-        
+        if (gameObject.tag == "Animals")
+        {
+            anim.GetComponent<Animator>();
+            BGM.GetComponent<AudioSource>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (gameObject.tag == "Props")
+        {
+            transform.Rotate(0, 2, 0);
+        }
     }
 
     void Update()
     {
-        
+        if (Tutorial_AnimalsControl.isDestory || Tutorial_Props.isDestory)
+        {
+            Destroy(gameObject);
+            Tutorial_AnimalsControl.isDestory = false;
+            Tutorial_Props.isDestory = false;
+        }
+
+        if (gameObject.tag == "Animals")
+        {
+            if (Tutorial_AnimalsControl.isAnimator)
+            {
+                anim.SetBool("Wave", true);
+                Tutorial_AnimalsControl.isAnimator = false;
+            }
+            else
+            {
+                anim.SetBool("Wave", false);
+            }
+
+            if (Tutorial_AnimalsControl.isCall)
+            {
+                BGM.Play();
+                Tutorial_AnimalsControl.isCall = false;
+            }
+        }
     }
 }
