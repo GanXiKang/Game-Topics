7a29dd9908584822adeeeaf2648edc27afb8a277
GanXiKang <gan0612xk@hotmail.com>
2023-11-23 01:14:36 +0800

    feat ： 調整時間

--- Assets/Script/MainMenu/Tutorial_PerfabControl.cs
+++ Assets/Script/MainMenu/Tutorial_PerfabControl.cs
@@ -38,7 +38,7 @@ public class Tutorial_PerfabControl : MonoBehaviour
             if (Tutorial_AnimalsControl.isAnimator)
             {
                 anim.SetBool("Wave", true);
-                Invoke("FalseByAnimator", 3f);
+                Invoke("FalseByAnimator", 2.5f);
                 Tutorial_AnimalsControl.isAnimator = false;
             }
 
