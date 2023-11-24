269b17658b9c1125a65470a197f2d7eccceed8af
GanXiKang <gan0612xk@hotmail.com>
2023-11-24 22:07:18 +0800

    feat ： 寫在一開始生產這

--- Assets/Script/MainGame/Instantiate/InstantiatePointControl.cs
+++ Assets/Script/MainGame/Instantiate/InstantiatePointControl.cs
@@ -7,11 +7,15 @@ public class InstantiatePointControl : MonoBehaviour
     public GameObject[] insPoint = new GameObject[4];
     public GameObject[] animals = new GameObject[12];
 
-    public static bool isStart = true;
+    public static bool isStart;
     void Start()
     {
         if (isStart)
         {
+            DiceControl.P1_totalNum = 0;
+            DiceControl.P2_totalNum = 0;
+            DiceControl.P3_totalNum = 0;
+            DiceControl.P4_totalNum = 0;
             Instantiate(animals[Menu_ChoosePlayer.whyP1 - 1], insPoint[0].transform.position, insPoint[0].transform.rotation);
             Instantiate(animals[Menu_ChoosePlayer.whyP2 - 1], insPoint[1].transform.position, insPoint[1].transform.rotation);
             if (Menu_ChoosePlayer.whoPlay >= 3)
