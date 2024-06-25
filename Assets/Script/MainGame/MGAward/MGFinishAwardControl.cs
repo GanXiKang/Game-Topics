5c7d1704990c61b7d5b44d19a02ffbca12496868
GanXiKang <gan0612xk@hotmail.com>
2024-06-26 01:06:40 +0800

    fix： delete code

--- Assets/Script/MainGame/MGAward/MGFinishAwardControl.cs
+++ Assets/Script/MainGame/MGAward/MGFinishAwardControl.cs
@@ -136,7 +136,6 @@ public class MGFinishAwardControl : MonoBehaviour
                             case 1:
                                 PropsControl.P1Props[6] += 1;
                                 systemText.text = "勝利獲得" + PropsControl.propsName[6];
-                                systemText.fontSize = 15;
                                 SystemTestTextControl.isTimer = true;
                                 break;
 
@@ -145,14 +144,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P1Props[1] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[1];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P1Props[2] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[2];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -162,14 +159,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P1Props[4] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[4];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P1Props[5] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[5];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -190,7 +185,6 @@ public class MGFinishAwardControl : MonoBehaviour
                             case 1:
                                 PropsControl.P2Props[6] += 1;
                                 systemText.text = "勝利獲得" + PropsControl.propsName[6];
-                                systemText.fontSize = 15;
                                 SystemTestTextControl.isTimer = true;
                                 break;
 
@@ -199,14 +193,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P2Props[1] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[1];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P2Props[2] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[2];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -216,14 +208,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P2Props[4] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[4];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P2Props[5] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[5];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -244,7 +234,6 @@ public class MGFinishAwardControl : MonoBehaviour
                             case 1:
                                 PropsControl.P3Props[6] += 1;
                                 systemText.text = "勝利獲得" + PropsControl.propsName[6];
-                                systemText.fontSize = 15;
                                 SystemTestTextControl.isTimer = true;
                                 break;
 
@@ -253,14 +242,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P3Props[1] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[1];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P3Props[2] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[2];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -270,14 +257,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P3Props[4] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[4];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P3Props[5] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[5];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -298,7 +283,6 @@ public class MGFinishAwardControl : MonoBehaviour
                             case 1:
                                 PropsControl.P4Props[6] += 1;
                                 systemText.text = "勝利獲得" + PropsControl.propsName[6];
-                                systemText.fontSize = 15;
                                 SystemTestTextControl.isTimer = true;
                                 break;
 
@@ -307,14 +291,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P4Props[1] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[1];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P4Props[2] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[2];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
@@ -324,14 +306,12 @@ public class MGFinishAwardControl : MonoBehaviour
                                 {
                                     PropsControl.P4Props[4] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[4];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 else
                                 {
                                     PropsControl.P4Props[5] += 1;
                                     systemText.text = "勝利獲得" + PropsControl.propsName[5];
-                                    systemText.fontSize = 15;
                                     SystemTestTextControl.isTimer = true;
                                 }
                                 break;
